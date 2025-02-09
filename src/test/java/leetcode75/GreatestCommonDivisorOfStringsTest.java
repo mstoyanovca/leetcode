9debd4c9e5018f2f7dc164590f4dce0ebc9e1d97
mstoyanovca <mstoyanovca@gmail.com>
2025-02-09 11:54:07 -0500

    completed example2

---
+++ src/test/java/leetcode75/GreatestCommonDivisorOfStringsTest.java
@@ -0,0 +1,29 @@
+package leetcode75;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreatestCommonDivisorOfStringsTest {
+    GreatestCommonDivisorOfStrings greatestCommonDivisorOfStrings = new GreatestCommonDivisorOfStrings();
+
+    @Test
+    void example1() {
+        assertEquals("ABC", greatestCommonDivisorOfStrings.gcdOfStrings("ABCABC", "ABC"));
+    }
+
+    @Test
+    void example2() {
+        assertEquals("AB", greatestCommonDivisorOfStrings.gcdOfStrings("ABABAB", "ABAB"));
+    }
+
+    @Test
+    void example3() {
+        assertEquals("", greatestCommonDivisorOfStrings.gcdOfStrings("LEET", "CODE"));
+    }
+
+    @Test
+    void example4() {
+        assertEquals("TAUXX", greatestCommonDivisorOfStrings.gcdOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"));
+    }
+}
