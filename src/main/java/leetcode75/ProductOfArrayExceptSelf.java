656cc659414b1a814449139a8b0dd23fb342f701
mstoyanovca <mstoyanovca@gmail.com>
2025-02-10 18:57:46 -0500

    solved example8

--- src/main/java/leetcode75/ProductOfArrayExceptSelf.java
+++ src/main/java/leetcode75/ProductOfArrayExceptSelf.java
@@ -2,23 +2,22 @@ package leetcode75;
 
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] numbers) {
-        int[] result = new int[numbers.length];
+        int n = numbers.length;
+        int[] prefixes = new int[n];
+        int[] suffixes = new int[n];
 
-        for (int i = 0; i < numbers.length; i++) {
-            int temp = numbers[i];
-            numbers[i] = 1;
-            result[i] = product(numbers);
-            numbers[i] = temp;
+        for (int i = 0; i < n; i++) {
+            prefixes[i] = i == 0 ? numbers[0] : prefixes[i - 1] * numbers[i];
+            int j = n - i - 1;
+            suffixes[j] = j == n - 1 ? numbers[n - 1] : numbers[j] * suffixes[j + 1];
         }
 
-        return result;
-    }
-
-    private int product(int[] temp) {
-        int product = 1;
-        for (int i : temp) {
-            product = product * i;
+        for (int i = 0; i < n; i++) {
+            int prefix = i == 0 ? 1 : prefixes[i - 1];
+            int suffix = i == n - 1 ? 1 : suffixes[i + 1];
+            numbers[i] = prefix * suffix;
         }
-        return product;
+
+        return numbers;
     }
 }
