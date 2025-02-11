cddf11665376dd4d7cf06b6f7c6ca0492b092c1c
mstoyanovca <mstoyanovca@gmail.com>
2025-02-10 21:26:32 -0500

    solved example8

--- src/test/java/leetcode75/ProductOfArrayExceptSelfTest.java
+++ src/test/java/leetcode75/ProductOfArrayExceptSelfTest.java
@@ -2,20 +2,18 @@ package leetcode75;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class ProductOfArrayExceptSelfTest {
     private final ProductOfArrayExceptSelf productOfArrayExceptSelf = new ProductOfArrayExceptSelf();
 
     @Test
     void example1() {
-        assertTrue(Arrays.equals(productOfArrayExceptSelf.productExceptSelf(new int[]{1, 2, 3, 4}), new int[]{24, 12, 8, 6}));
+        assertArrayEquals(productOfArrayExceptSelf.productExceptSelf(new int[]{1, 2, 3, 4}), new int[]{24, 12, 8, 6});
     }
 
     @Test
     void example2() {
-        assertTrue(Arrays.equals(productOfArrayExceptSelf.productExceptSelf(new int[]{-1, 1, 0, -3, 3}), new int[]{0, 0, 9, 0, 0}));
+        assertArrayEquals(productOfArrayExceptSelf.productExceptSelf(new int[]{-1, 1, 0, -3, 3}), new int[]{0, 0, 9, 0, 0});
     }
 }
