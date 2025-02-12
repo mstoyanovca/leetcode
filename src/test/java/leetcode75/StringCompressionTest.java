0846a1e9d8739c2aa11a364321c51e6c37a820c0
mstoyanovca <mstoyanovca@gmail.com>
2025-02-11 20:32:57 -0500

    solved problem 9

---
+++ src/test/java/leetcode75/StringCompressionTest.java
@@ -0,0 +1,29 @@
+package leetcode75;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCompressionTest {
+    private final StringCompression stringCompression = new StringCompression();
+
+    @Test
+    void example1() {
+        assertEquals(6, stringCompression.compress(new char[]{'a', 'a', 'b', 'b', 'c', 'c', 'c'}));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(1, stringCompression.compress(new char[]{'a'}));
+    }
+
+    @Test
+    void example3() {
+        assertEquals(4, stringCompression.compress(new char[]{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}));
+    }
+
+    @Test
+    void example4() {
+        assertEquals(6, stringCompression.compress(new char[]{'a', 'a', 'a', 'b', 'b', 'a', 'a'}));
+    }
+}
