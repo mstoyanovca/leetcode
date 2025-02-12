302070f353321d1a74d27480fc07af1bfc004e4f
mstoyanovca <mstoyanovca@gmail.com>
2025-02-11 21:59:54 -0500

    solved q11

---
+++ src/test/java/leetcode75/IsSubsequenceTest.java
@@ -0,0 +1,20 @@
+package leetcode75;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IsSubsequenceTest {
+    private final IsSubsequence isSubsequence = new IsSubsequence();
+
+    @Test
+    void example1() {
+        assertTrue(isSubsequence.isSubsequence("abc", "ahbgdc"));
+    }
+
+    @Test
+    void example2() {
+        assertFalse(isSubsequence.isSubsequence("axc", "ahbgdc"));
+    }
+}
