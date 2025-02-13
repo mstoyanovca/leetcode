bbdf18ae4274bd0f151323627c53badd2476e2ce
mstoyanovca <mstoyanovca@gmail.com>
2025-02-12 21:05:43 -0500

    solved q13

---
+++ src/test/java/leetcode75/MaxNumberOfKSumPairsTest.java
@@ -0,0 +1,34 @@
+package leetcode75;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MaxNumberOfKSumPairsTest {
+    private final MaxNumberOfKSumPairs maxNumberOfKSumPairs = new MaxNumberOfKSumPairs();
+
+    @Test
+    void example1() {
+        assertEquals(2, maxNumberOfKSumPairs.maxOperations(new int[]{1, 2, 3, 4}, 5));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(1, maxNumberOfKSumPairs.maxOperations(new int[]{3, 1, 3, 4, 3}, 6));
+    }
+
+    @Test
+    void example3() {
+        assertEquals(2, maxNumberOfKSumPairs.maxOperations(new int[]{4, 4, 1, 3, 1, 3, 2, 2, 5, 5, 1, 5, 2, 1, 2, 3, 5, 4}, 2));
+    }
+
+    @Test
+    void example4() {
+        assertEquals(0, maxNumberOfKSumPairs.maxOperations(new int[]{3, 5, 1, 5}, 2));
+    }
+
+    @Test
+    void example5() {
+        assertEquals(2, maxNumberOfKSumPairs.maxOperations(new int[]{2, 2, 2, 3, 1, 1, 4, 1}, 4));
+    }
+}
