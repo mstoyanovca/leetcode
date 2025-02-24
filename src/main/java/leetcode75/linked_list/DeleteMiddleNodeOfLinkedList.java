ea061bc9d0f617898e006fc6b4d05edb4823b32d
mstoyanovca <mstoyanovca@gmail.com>
2025-02-23 22:44:14 -0500

    solved problem 30

--- src/main/java/leetcode75/linked_list/DeleteMiddleNodeOfLinkedList.java
+++ src/main/java/leetcode75/linked_list/DeleteMiddleNodeOfLinkedList.java
@@ -1,7 +1,5 @@
 package leetcode75.linked_list;
 
-import java.util.Objects;
-
 // problem 29
 public class DeleteMiddleNodeOfLinkedList {
     public ListNode deleteMiddle(ListNode head) {
@@ -26,31 +24,5 @@ public class DeleteMiddleNodeOfLinkedList {
         }
 
         return head;
-    }
-
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (o == null || getClass() != o.getClass()) return false;
-            ListNode listNode = (ListNode) o;
-            return val == listNode.val && Objects.equals(next, listNode.next);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(val, next);
-        }
     }
 }
