60d1bd0c36b897eb13aa89925b8a561842dab921
mstoyanovca <mstoyanovca@gmail.com>
2025-03-01 18:25:39 -0500

    solved problem 37

--- src/main/java/leetcode75/binary_tree_dfs/LongestZigZagPath.java
+++ src/main/java/leetcode75/binary_tree_dfs/LongestZigZagPath.java
@@ -2,25 +2,34 @@ package leetcode75.binary_tree_dfs;
 
 // problem 37:
 public class LongestZigZagPath {
-    private enum Direction {LEFT, RIGHT}
+    /*private enum Direction {LEFT, RIGHT}
 
     public int longestZigZag(TreeNode root) {
-        if (root == null) return 0;
-        /*return Math.max(
-                Math.max(longestZigZag(root.left),
-                        longestZigZag(root.right)),
-                Math.max(root.left != null ? 1 + dfs(root.left, Direction.LEFT) : 0,
-                        root.right != null ? 1 + dfs(root.right, Direction.RIGHT) : 0));*/
-        return Math.max(
-                root.left != null ? 1 + dfs(root.left, Direction.LEFT) : 0,
-                root.right != null ? 1 + dfs(root.right, Direction.RIGHT) : 0);
+        return Math.max(dfs(root, Direction.LEFT), dfs(root, Direction.RIGHT));
     }
 
     private int dfs(TreeNode node, Direction direction) {
         if (node == null) return 0;
         Direction nextDirection = direction == Direction.LEFT ? Direction.RIGHT : Direction.LEFT;
         TreeNode nextNode = nextDirection == Direction.LEFT ? node.left : node.right;
-        // this node might be a root of a longer zigzag path:
-        return nextNode != null ? 1 + dfs(nextNode, nextDirection) : 0;
+        TreeNode nextRoot = nextDirection == Direction.LEFT ? node.right : node.left;
+        return Math.max(
+                nextNode != null ? 1 + dfs(nextNode, nextDirection) : 0,
+                // this node might be a root of a new path:
+                dfs(nextRoot, direction));
+    }*/
+    public int longestZigZag(TreeNode root) {
+        return Math.max(dfs(root, "LEFT"), dfs(root, "RIGHT"));
+    }
+
+    private int dfs(TreeNode node, String direction) {
+        if (node == null) return 0;
+        String nextDirection = direction.equals("LEFT") ? "RIGHT" : "LEFT";
+        TreeNode nextNode = nextDirection.equals("LEFT") ? node.left : node.right;
+        TreeNode nextRoot = nextDirection.equals("LEFT") ? node.right : node.left;
+        return Math.max(
+                nextNode != null ? 1 + dfs(nextNode, nextDirection) : 0,
+                // this node might be a root of a new path:
+                dfs(nextRoot, direction));
     }
 }
