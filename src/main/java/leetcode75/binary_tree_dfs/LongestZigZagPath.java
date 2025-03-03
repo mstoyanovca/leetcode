ba2c2c3edf2c9c174b69acf5aa6b4c9e2f0dc4ae
mstoyanovca <mstoyanovca@gmail.com>
2025-03-02 20:32:55 -0500

    solved problem 37

--- src/main/java/leetcode75/binary_tree_dfs/LongestZigZagPath.java
+++ src/main/java/leetcode75/binary_tree_dfs/LongestZigZagPath.java
@@ -2,34 +2,18 @@ package leetcode75.binary_tree_dfs;
 
 // problem 37:
 public class LongestZigZagPath {
-    /*private enum Direction {LEFT, RIGHT}
+    private enum Direction {LEFT, RIGHT}
 
-    public int longestZigZag(TreeNode root) {
-        return Math.max(dfs(root, Direction.LEFT), dfs(root, Direction.RIGHT));
-    }
-
-    private int dfs(TreeNode node, Direction direction) {
-        if (node == null) return 0;
-        Direction nextDirection = direction == Direction.LEFT ? Direction.RIGHT : Direction.LEFT;
-        TreeNode nextNode = nextDirection == Direction.LEFT ? node.left : node.right;
-        TreeNode nextRoot = nextDirection == Direction.LEFT ? node.right : node.left;
-        return Math.max(
-                nextNode != null ? 1 + dfs(nextNode, nextDirection) : 0,
-                // this node might be a root of a new path:
-                dfs(nextRoot, direction));
-    }*/
     public int longestZigZag(TreeNode root) {
         return Math.max(dfs(root, "LEFT"), dfs(root, "RIGHT"));
     }
 
     private int dfs(TreeNode node, String direction) {
         if (node == null) return 0;
-        String nextDirection = direction.equals("LEFT") ? "RIGHT" : "LEFT";
-        TreeNode nextNode = nextDirection.equals("LEFT") ? node.left : node.right;
-        TreeNode nextRoot = nextDirection.equals("LEFT") ? node.right : node.left;
-        return Math.max(
-                nextNode != null ? 1 + dfs(nextNode, nextDirection) : 0,
-                // this node might be a root of a new path:
-                dfs(nextRoot, direction));
+        if (direction.equals("LEFT")) {
+            return Math.max(node.left != null ? 1 + dfs(node.left, "RIGHT") : 0, dfs(node.left, "LEFT"));
+        } else {
+            return Math.max(node.right != null ? 1 + dfs(node.right, "LEFT") : 0, dfs(node.right, "RIGHT"));
+        }
     }
 }
