40c7bdf14cee2651900a1a05e053b91d14446d6c
mstoyanovca <mstoyanovca@gmail.com>
2025-03-02 21:18:08 -0500

    solved problem 37

--- src/main/java/leetcode75/binary_tree_dfs/LongestZigZagPath.java
+++ src/main/java/leetcode75/binary_tree_dfs/LongestZigZagPath.java
@@ -1,26 +1,26 @@
 package leetcode75.binary_tree_dfs;
 
-// problem 37:
+// problem 37;
+// leetcode website doesn't recognize enumerations;
 public class LongestZigZagPath {
-    private int maxSteps = 0;
-
     public int longestZigZag(TreeNode root) {
         return Math.max(dfs(root, "LEFT", 0), dfs(root, "RIGHT", 0));
     }
 
     private int dfs(TreeNode node, String direction, int currentSteps) {
         if (node == null) return 0;
-        maxSteps = Math.max(maxSteps, currentSteps);
         if (direction.equals("LEFT")) {
-            dfs(node.left, "RIGHT", currentSteps + 1);
-            // it might be a root of a new path:
-            dfs(node.left, "LEFT", 0);
+            return Math.max(
+                    Math.max(
+                            dfs(node.left, "RIGHT", currentSteps + 1),
+                            dfs(node.left, "LEFT", 0)),
+                    currentSteps);
         } else {
-            dfs(node.right, "LEFT", currentSteps + 1);
-            // it might be a root of a new path:
-            dfs(node.right, "RIGHT", 0);
+            return Math.max(
+                    Math.max(
+                            dfs(node.right, "LEFT", currentSteps + 1),
+                            dfs(node.right, "RIGHT", 0)),
+                    currentSteps);
         }
-
-        return maxSteps;
     }
 }
