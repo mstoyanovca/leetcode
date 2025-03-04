8a49d735cfd2f4554ed5c0e7a972a26279b95ab3
mstoyanovca <mstoyanovca@gmail.com>
2025-03-03 20:54:29 -0500

    solved problem 38

---
+++ src/main/java/leetcode75/binary_tree_dfs/LowestCommonAncestor.java
@@ -0,0 +1,17 @@
+package leetcode75.binary_tree_dfs;
+
+// problem 38
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p.equals(root) || q.equals(root)) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+}
