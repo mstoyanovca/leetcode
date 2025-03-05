a0b471ade6c41bf9470c6ecb3d593ad0d4c574c8
mstoyanovca <mstoyanovca@gmail.com>
2025-03-04 19:57:31 -0500

    solved problem 39

---
+++ src/main/java/leetcode75/binary_tree_bfs/BinaryTreeRightSideView.java
@@ -0,0 +1,56 @@
+package leetcode75.binary_tree_bfs;
+
+import leetcode75.binary_tree_dfs.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * DFS stands for Depth First Search;
+ * DFS builds the tree subtree by subtree;
+ * DFS uses stack data structure;
+ * It works on the concept of LIFO;
+ * DFS is more suitable when there are solutions away from source.
+ * BFS stands for Breadth First Search;
+ * BFS builds the tree level by level;
+ * BFS uses Queue data structure for finding the shortest path;
+ * It works on the concept of FIFO;
+ * BFS is more suitable for searching vertices closer to the given source;
+ */
+public class BinaryTreeRightSideView {
+    // problem 39, Queue/FIFO:
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int depth = queue.size();
+            for (int i = 0; i < depth; i++) {
+                TreeNode node = queue.remove();
+                if (i == 0) result.add(node.val);
+                if (node.right != null) queue.add(node.right);
+                if (node.left != null) queue.add(node.left);
+            }
+        }
+
+        return result;
+    }
+
+    // problem 39, recursive:
+    public List<Integer> rightSideViewRecursive(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        bfs(root, result, 0);
+        return result;
+    }
+
+    private void bfs(TreeNode node, List<Integer> result, int depth) {
+        if (node == null) return;
+        if (result.size() == depth) result.add(node.val);
+        bfs(node.right, result, depth + 1);
+        bfs(node.left, result, depth + 1);
+    }
+}
--- src/main/java/leetcode75/binary_tree_dfs/TreeNode.java
+++ src/main/java/leetcode75/binary_tree_dfs/TreeNode.java
@@ -3,17 +3,17 @@ package leetcode75.binary_tree_dfs;
 import java.util.Objects;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-    TreeNode(int val) {
+    public TreeNode(int val) {
         this.val = val;
         left = null;
         right = null;
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
