9d0b698a3e9517a452554d074dfc00fa0ad80d8b
mstoyanovca <mstoyanovca@gmail.com>
2025-03-26 18:47:41 -0400

    created problem 52

--- src/main/java/leetcode75/heap/TotalCostToHireKWorkers.java
+++ src/main/java/leetcode75/heap/TotalCostToHireKWorkers.java
@@ -1,36 +1,40 @@
 package leetcode75.heap;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.stream.IntStream;
 
 // problem 52:
 public class TotalCostToHireKWorkers {
     public long totalCost(int[] costs, int k, int candidates) {
-        // hire k workers, from # of either the first, or the last candidates
+        // hire k workers, from # of either the first, or the last candidates, the smaller index breaks the tie;
         int totalCost = 0;
         List<List<Integer>> indexToCost = createIndexToCost(costs);
-        PriorityQueue<List<Integer>> minHeapLeft = new PriorityQueue<>();
-        PriorityQueue<List<Integer>> minHeapRight = new PriorityQueue<>();
+        PriorityQueue<List<Integer>> minHeapLeft = new PriorityQueue<>(Comparator.comparingInt(a -> a.get(1)));
+        PriorityQueue<List<Integer>> minHeapRight = new PriorityQueue<>(Comparator.comparingInt(a -> a.get(1)));
 
-        List<Integer> x = indexToCost.get(0);
-        minHeapLeft.add(x);
-        for (int i = 0; i < candidates; i++) minHeapLeft.add(indexToCost.get(i));
-        for (int i = costs.length - candidates; i < costs.length; i++) minHeapRight.add(indexToCost.get(i));
+        populateHeap(0, candidates, indexToCost, minHeapLeft);
+        populateHeap(costs.length - candidates, costs.length, indexToCost, minHeapRight);
+        // for (int i = 0; i < candidates; i++) minHeapLeft.add(indexToCost.get(i));
+        // for (int i = costs.length - candidates; i < costs.length; i++) minHeapRight.add(indexToCost.get(i));
 
         for (int i = 0; i < k; i++) {
             if (!minHeapLeft.isEmpty() && !minHeapRight.isEmpty()) {
                 List<Integer> worker;
-                if (minHeapLeft.peek().get(1) < minHeapRight.peek().get(1)) {
+                if (minHeapLeft.peek().get(1) <= minHeapRight.peek().get(1)) {
                     worker = minHeapLeft.remove();
-                } else if (minHeapLeft.peek().get(1) > minHeapRight.peek().get(1)) {
-                    worker = minHeapRight.remove();
+                    indexToCost.remove(worker);
+                    minHeapLeft.clear();
+                    populateHeap(0, candidates, indexToCost, minHeapLeft);
                 } else {
-                    // minHeapLeft.peek().get(1) == minHeapRight.peek().get(1), the smaller index breaks the tie:
-                    worker = minHeapLeft.remove();
+                    worker = minHeapRight.remove();
+                    indexToCost.remove(worker);
+                    minHeapRight.clear();
+                    populateHeap(costs.length - candidates, costs.length, indexToCost, minHeapRight);
                 }
                 totalCost += worker.get(1);
+                int x = 0;
             }
         }
 
@@ -38,11 +42,17 @@ public class TotalCostToHireKWorkers {
     }
 
     private List<List<Integer>> createIndexToCost(int[] costs) {
-        return IntStream.range(0, costs.length - 1).mapToObj(i -> {
-            List<Integer> list = new ArrayList<>();
-            list.add(i);
-            list.add(costs[i]);
-            return list;
-        }).toList();
+        List<List<Integer>> indexToCost = new ArrayList<>();
+        for (int i = 0; i < costs.length; i++) {
+            List<Integer> worker = new ArrayList<>();
+            worker.add(i);
+            worker.add(costs[i]);
+            indexToCost.add(worker);
+        }
+        return indexToCost;
+    }
+
+    private void populateHeap(int start, int end, List<List<Integer>> indexToCost, PriorityQueue<List<Integer>> heap) {
+        for (int i = start; i < end; i++) heap.add(indexToCost.get(i));
     }
 }
