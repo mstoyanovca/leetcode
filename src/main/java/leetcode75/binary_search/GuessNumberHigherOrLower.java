bc3d6d51b7175c6531edb40fd9a1fe4769b43bb0
mstoyanovca <mstoyanovca@gmail.com>
2025-03-28 20:08:53 -0400

    solved problem 54

--- src/main/java/leetcode75/binary_search/GuessNumberHigherOrLower.java
+++ src/main/java/leetcode75/binary_search/GuessNumberHigherOrLower.java
@@ -4,18 +4,18 @@ package leetcode75.binary_search;
 public class GuessNumberHigherOrLower {
     public int guessNumber(int n) {
         if (n == 1) return 1;
-        return dfs(n / 2, 1, n);
+        return binarySearch(n / 2, 1, n);
     }
 
-    private int dfs(int guess, int lowerLimit, int upperLimit) {
+    // recursive:
+    private int binarySearch(int guess, int lowerLimit, int upperLimit) {
         return switch (guess(guess)) {
             case -1 ->
-                // your guess is higher than the number I picked:
-                    dfs(guess / 2, lowerLimit, guess);
+                // your guess is higher than the number I picked, it rounds down:
+                    binarySearch(guess / 2, lowerLimit, guess);
             case 1 ->
-                // your guess is lower than the number I picked
-                // dfs((int) (guess + Math.ceil((double) (upperLimit - guess) / 2)), guess, upperLimit);
-                    dfs(guess + (upperLimit - guess + 1) / 2, guess, upperLimit);
+                // your guess is lower than the number I picked, add 1 to round up:
+                    binarySearch(guess + (upperLimit - guess + 1) / 2, guess, upperLimit);
             default ->
                 // your guess is equal to the number I picked:
                     guess;
---
+++ src/main/java/leetcode75/binary_search/SpellsAndPotions.java
@@ -0,0 +1,32 @@
+package leetcode75.binary_search;
+
+import java.util.Arrays;
+
+// problem 54:
+public class SpellsAndPotions {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length;
+        int m = potions.length;
+        int[] pairs = new int[n];
+
+        Arrays.sort(potions);
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = m - 1;
+
+            // two pointers technique:
+            while (left <= right) {
+                int current = left + (right - left) / 2;
+                if ((long) spells[i] * potions[current] >= success) {
+                    right = --current;
+                } else {
+                    left = ++current;
+                }
+            }
+
+            pairs[i] = m - left;
+        }
+
+        return pairs;
+    }
+}
