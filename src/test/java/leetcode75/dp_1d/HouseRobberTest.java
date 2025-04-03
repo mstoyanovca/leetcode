e96ae1496877453b56fcabb8c9f2b1a893c816b0
mstoyanovca <mstoyanovca@gmail.com>
2025-04-02 21:11:24 -0400

    solved problem 61

---
+++ src/test/java/leetcode75/dp_1d/HouseRobberTest.java
@@ -0,0 +1,29 @@
+package leetcode75.dp_1d;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HouseRobberTest {
+    private final HouseRobber robber = new HouseRobber();
+
+    @Test
+    void example1() {
+        assertEquals(4, robber.rob(new int[]{1, 2, 3, 1}));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(12, robber.rob(new int[]{2, 7, 9, 3, 1}));
+    }
+
+    @Test
+    void example3() {
+        assertEquals(0, robber.rob(new int[]{0}));
+    }
+
+    @Test
+    void example4() {
+        assertEquals(4, robber.rob(new int[]{2, 1, 1, 2}));
+    }
+}
