6bf2c8829900e849d97a5b04e20646fc187ab02a
mstoyanovca <mstoyanovca@gmail.com>
2025-04-09 21:21:17 -0400

    cleaning

--- src/main/java/leetcode75/array_string/MergeStringsAlternately.java
+++ src/main/java/leetcode75/array_string/MergeStringsAlternately.java
@@ -4,7 +4,7 @@ public class MergeStringsAlternately {
     public String mergeAlternately(String word1, String word2) {
         final int length1 = word1.length();
         final int length2 = word2.length();
-        // both strings might be with the same length:
+        // both strings might not be with the same length:
         int minLength = Math.min(length1, length2);
         String longerString = length1 > length2 ? word1 : word2;
         StringBuilder stringBuilder = new StringBuilder();
