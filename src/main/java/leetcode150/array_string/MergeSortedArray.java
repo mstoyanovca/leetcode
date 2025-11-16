ecc2c7bf203609cc8e4fc9ebce1a524e665fa8cd
mstoyanovca <mstoyanovca@gmail.com>
2025-11-15 20:03:09 -0500

    completed MergeSortedArray

--- src/main/java/leetcode150/array_string/MergeSortedArray.java
+++ src/main/java/leetcode150/array_string/MergeSortedArray.java
@@ -1,4 +1,31 @@
 package leetcode150.array_string;
 
 public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int inserts = 0;
+
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < (m + n); i++) {
+                if (nums1[i] > nums2[j]) {
+                    inserts++;
+                    insert(nums1, i, nums2[j]);
+                    break;
+                }
+                if (i >= m + inserts && nums1[i] == 0) {
+                    inserts++;
+                    insert(nums1, i, nums2[j]);
+                    break;
+                }
+
+            }
+        }
+    }
+
+    public void insert(int[] numbers, int position, int number) {
+        for (int i = position; i < numbers.length; i++) {
+            int acc = numbers[i];
+            numbers[i] = number;
+            number = acc;
+        }
+    }
 }
--- src/test/java/leetcode150/array_string/MergeSortedArrayTest.java
+++ src/test/java/leetcode150/array_string/MergeSortedArrayTest.java
@@ -1,4 +1,28 @@
 package leetcode150.array_string;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class MergeSortedArrayTest {
+    @Test
+    public void example1Test() {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        (new MergeSortedArray()).merge(nums1, 3, new int[]{2, 5, 6}, 3);
+        assertArrayEquals(new int[]{1, 2, 2, 3, 5, 6}, nums1);
+    }
+
+    @Test
+    public void example2Test() {
+        int[] nums1 = {-1, 3, 0, 0, 0, 0, 0};
+        (new MergeSortedArray()).merge(nums1, 2, new int[]{0, 0, 1, 2, 3}, 5);
+        assertArrayEquals(new int[]{-1, 0, 0, 1, 2, 3, 3}, nums1);
+    }
+
+    @Test
+    public void insertTest() {
+        int[] nums = new int[]{1, 2, 3, 0, 0, 0};
+        (new MergeSortedArray()).insert(nums, 0, 2);
+        assertArrayEquals(new int[]{2, 1, 2, 3, 0, 0}, nums);
+    }
 }
