911f706dba4624182243a949d39e57d21fcfb660
mstoyanovca <mstoyanovca@gmail.com>
2025-11-19 20:56:14 -0500

    solved BuySellStock

--- src/main/java/leetcode150/array_string/BuySellStock1.java
+++ src/main/java/leetcode150/array_string/BuySellStock1.java
@@ -10,16 +10,21 @@ public class BuySellStock1 {
         // space complexity O(1)
         // looking for a global extremum;
         while (right < prices.length) {
+            // slide the left pointer downhill to the next local minimum:
             while (left + 1 < prices.length && prices[left + 1] < prices[left]) {
                 left++;
+                // can't sell before buy:
                 right = left;
             }
+            // slide the right pointer uphill to the next local maximum:
             while (right + 1 < prices.length && prices[right + 1] > prices[right]) {
                 right++;
             }
             maxProfit = Math.max(prices[right] - prices[left], maxProfit);
             if (right + 1 < prices.length) {
+                // move the right pointer downhill, expecting new local maximum after that:
                 right++;
+                // it might be a global minimum:
                 if (prices[right] < prices[left]) left = right;
             } else {
                 break;
