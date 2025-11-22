72e8e8df632a89969ab27fdebb6defb84b1290ac
mstoyanovca <mstoyanovca@gmail.com>
2025-11-22 16:12:19 -0500

    JumpGame2

--- src/main/java/leetcode150/array_string/JumpGame2.java
+++ src/main/java/leetcode150/array_string/JumpGame2.java
@@ -2,6 +2,31 @@ package leetcode150.array_string;
 
 public class JumpGame2 {
     public int jump(int[] nums) {
+        return 0;
+    }
+
+    public int jump2(int[] nums) {
+        if (nums.length == 1) return 0;
+
+        int n = nums.length;
+        int jumps = 0;
+        int maxRight = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] + i >= maxRight) {
+                jumps++;
+                maxRight = nums[i] + i;
+            }
+            if (i + maxRight >= n - 1) {
+                jumps++;
+                break;
+            }
+        }
+
+        return jumps;
+    }
+
+    public int jump3(int[] nums) {
         int n = nums.length;
         int left = 0;
         int right = 0;
