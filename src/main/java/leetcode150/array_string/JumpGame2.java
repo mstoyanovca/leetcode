9402fd32b40f4a8f677e92e39be75214830b7380
mstoyanovca <mstoyanovca@gmail.com>
2025-11-22 17:12:42 -0500

    solved JumpGame2

--- src/main/java/leetcode150/array_string/JumpGame2.java
+++ src/main/java/leetcode150/array_string/JumpGame2.java
@@ -2,7 +2,20 @@ package leetcode150.array_string;
 
 public class JumpGame2 {
     public int jump(int[] nums) {
-        return 0;
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
+        int jumps = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            right = Math.max(right, i + nums[i]);
+            if (left == i) {
+                jumps++;
+                left = right;
+            }
+        }
+
+        return jumps;
     }
 
     public int jump2(int[] nums) {
@@ -50,7 +63,6 @@ public class JumpGame2 {
                 left = maxRightIndex;
                 right = maxRightIndex;
                 maxRight = 0;
-                int x = 0;
             }
         }
 
