d5f6555259e5389bcda4677985f954856fabb9c2
mstoyanovca <mstoyanovca@gmail.com>
2025-11-23 13:18:53 -0500

    solved ProductOfArrayExceptSelf

--- src/main/java/leetcode150/array_string/ProductOfArrayExceptSelf.java
+++ src/main/java/leetcode150/array_string/ProductOfArrayExceptSelf.java
@@ -3,10 +3,48 @@ package leetcode150.array_string;
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
+        // optimized version for O(1) space complexity;
+        // use nums as prefix;
+        // use answer as suffix, which means calculate suffix first, before you spoil nums :);
         int[] prefix = new int[n];
         int[] suffix = new int[n];
         int[] answer = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                prefix[i] = 1;
+            } else if (i == 1) {
+                prefix[i] = nums[0];
+            } else {
+                prefix[i] = prefix[i - 1] * nums[i - 1];
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == n - 1) {
+                suffix[i] = 1;
+            } else if (i == n - 2) {
+                suffix[i] = nums[n - 1];
+            } else {
+                suffix[i] = suffix[i + 1] * nums[i + 1];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = prefix[i] * suffix[i];
+        }
+
+        return answer;
+    }
+
+    public int[] productExceptSelfBruteForce(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        int[] answer = new int[n];
+
+        // time complexity O(n)
+        // space complexity I think it's going to be O(n)
         for (int i = 0; i < n; i++) {
             if (i == 0) {
                 prefix[i] = 1;
