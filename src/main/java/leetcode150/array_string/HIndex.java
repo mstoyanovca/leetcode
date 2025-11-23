79cf4b79693918cb40037f67c31881f4550fd61f
mstoyanovca <mstoyanovca@gmail.com>
2025-11-23 09:08:39 -0500

    H-Index

--- src/main/java/leetcode150/array_string/HIndex.java
+++ src/main/java/leetcode150/array_string/HIndex.java
@@ -1,15 +1,26 @@
 package leetcode150.array_string;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HIndex {
     public int hIndex(int[] citations) {
         int n = citations.length;
+        Map<Integer, Integer> occurrences = new HashMap<>();
         int h = 0;
 
         Arrays.sort(citations);
-        for (int i = 0; i < n; i++) if (citations[i] == n - i) return citations[i];
+        for (int i = 0; i < n; i++) {
+            if (citations[i] == 0) continue;
+            int key = citations[i];
+            if (!occurrences.containsKey(key)) occurrences.put(key, n - i);
+            if (key >= occurrences.get(key)) {
+                h = occurrences.get(key);
+                break;
+            }
+        }
 
-        return h;
+        return h == 0 && !occurrences.isEmpty() ? 1 : h;
     }
 }
