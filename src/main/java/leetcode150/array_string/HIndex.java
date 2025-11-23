9545ff85b954e2ae59e1615786f6a98aed5c233a
mstoyanovca <mstoyanovca@gmail.com>
2025-11-23 10:35:44 -0500

    solved H-Index

--- src/main/java/leetcode150/array_string/HIndex.java
+++ src/main/java/leetcode150/array_string/HIndex.java
@@ -1,26 +1,26 @@
 package leetcode150.array_string;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class HIndex {
     public int hIndex(int[] citations) {
         int n = citations.length;
-        Map<Integer, Integer> occurrences = new HashMap<>();
         int h = 0;
 
         Arrays.sort(citations);
+        /*
+         * 0, 1, 3, 5, 6  h papers that have each been cited at least h times
+         *                papers <= citations
+         *                papersToCitation: 1->1, 3->3, 2->5, 1->6
+         * 2, 2, 2        2->2
+         */
         for (int i = 0; i < n; i++) {
             if (citations[i] == 0) continue;
-            int key = citations[i];
-            if (!occurrences.containsKey(key)) occurrences.put(key, n - i);
-            if (key >= occurrences.get(key)) {
-                h = occurrences.get(key);
-                break;
-            }
+            int numberOfCitations = citations[i];
+            int papers = Math.min(numberOfCitations, n - i); // 1, 3, 2, 1
+            h = Math.max(h, papers);
         }
 
-        return h == 0 && !occurrences.isEmpty() ? 1 : h;
+        return h;
     }
 }
