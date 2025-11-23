e4c0d91a8f48a617298a2af474585b0d232b9aad
mstoyanovca <mstoyanovca@gmail.com>
2025-11-23 13:22:23 -0500

    optimized ProductOfArrayExceptSelf

--- src/main/java/leetcode150/array_string/ProductOfArrayExceptSelf.java
+++ src/main/java/leetcode150/array_string/ProductOfArrayExceptSelf.java
@@ -3,11 +3,11 @@ package leetcode150.array_string;
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        // optimized version for O(1) space complexity;
+        // optimized version for O(1) space complexity:
         // use nums as prefix;
-        // use answer as suffix, which means calculate suffix first, before you spoil nums :);
+        // use answer as suffix, which means calculate suffix first, before you overwrite nums :);
         int[] prefix = new int[n];
-        int[] suffix = new int[n];
+        // int[] suffix = new int[n];
         int[] answer = new int[n];
 
         for (int i = 0; i < n; i++) {
@@ -22,16 +22,16 @@ public class ProductOfArrayExceptSelf {
 
         for (int i = n - 1; i >= 0; i--) {
             if (i == n - 1) {
-                suffix[i] = 1;
+                answer[i] = 1;
             } else if (i == n - 2) {
-                suffix[i] = nums[n - 1];
+                answer[i] = nums[n - 1];
             } else {
-                suffix[i] = suffix[i + 1] * nums[i + 1];
+                answer[i] = answer[i + 1] * nums[i + 1];
             }
         }
 
         for (int i = 0; i < n; i++) {
-            answer[i] = prefix[i] * suffix[i];
+            answer[i] = prefix[i] * answer[i];
         }
 
         return answer;
