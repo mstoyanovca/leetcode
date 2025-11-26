849910ac61114796ec72a4b5dd6b2ad3427c09ed
mstoyanovca <mstoyanovca@gmail.com>
2025-11-25 19:49:33 -0500

    sloved GasStation

---
+++ src/main/java/leetcode150/array_string/GasStation.java
@@ -0,0 +1,44 @@
+package leetcode150.array_string;
+
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        int currentGas = 0;
+        int right = findNextPossibleStartIndex(0, gas, cost);
+
+        while (right != -1) {
+            boolean loop = true;
+
+            for (int i = right; i < n; i++) {
+                currentGas += gas[i] - cost[i];
+                if (currentGas < 0) {
+                    right = findNextPossibleStartIndex(right + 1, gas, cost);
+                    currentGas = 0;
+                    loop = false;
+                    break;
+                }
+            }
+
+            if (loop) {
+                for (int i = 0; i < right; i++) {
+                    currentGas += gas[i] - cost[i];
+                    if (currentGas < 0) {
+                        right = findNextPossibleStartIndex(right + 1, gas, cost);
+                        currentGas = 0;
+                        loop = false;
+                        break;
+                    }
+                }
+
+                if (loop) break;
+            }
+        }
+
+        return right;
+    }
+
+    private int findNextPossibleStartIndex(int right, int[] gas, int[] cost) {
+        for (int i = right; i < gas.length; i++) if (gas[i] >= cost[i]) return i;
+        return -1;
+    }
+}
---
+++ src/test/java/leetcode150/array_string/GasStationTest.java
@@ -0,0 +1,22 @@
+package leetcode150.array_string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GasStationTest {
+    @Test
+    public void example1Test() {
+        assertEquals(3, new GasStation().canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}));
+    }
+
+    @Test
+    public void example2Test() {
+        assertEquals(-1, new GasStation().canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3}));
+    }
+
+    @Test
+    public void example3Test() {
+        assertEquals(0, new GasStation().canCompleteCircuit(new int[]{2}, new int[]{2}));
+    }
+}
