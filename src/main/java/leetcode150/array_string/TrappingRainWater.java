d2d5c0a7a86bdf0e461f67e0f532f6168cfa8918
mstoyanovca <mstoyanovca@gmail.com>
2025-11-28 21:27:09 -0500

    created TrappingRainWater

--- src/main/java/leetcode150/array_string/TrappingRainWater.java
+++ src/main/java/leetcode150/array_string/TrappingRainWater.java
@@ -3,29 +3,35 @@ package leetcode150.array_string;
 public class TrappingRainWater {
     public int trap(int[] height) {
         int n = height.length;
-        int left = 0;
+        int left = -1;
         int right = 0;
         int volume = 0;
 
-        while (right < n) {
-            // find a left wall:
-            while (left < n - 1 && height[left + 1] > height[left]) {
-                left++;
+        for (int i = 0; i < n; i++) {
+            // taller than its neighbours, it's a wall:
+            boolean firstElementPredicate = n > 1 && i == 0 && height[0] > height[1];
+            boolean middleElementPredicate = i > 0 && i < n - 1 && height[i] > height[i + 1] && height[i] > height[i - 1];
+            boolean lastElementPredicate = n > 1 && i == n - 1 && height[n - 1] > height[n - 2];
+            if (firstElementPredicate) {
+                left = 0;
+            } else if (middleElementPredicate) {
+                if (left == -1) {
+                    // it's the leftmost wall:
+                    left = i;
+                } else {
+                    // it's a right wall:
+                    right = i;
+                }
+            } else if (lastElementPredicate) {
+                right = n - 1;
             }
-            right = left;
-            // find a right wall:
-            while (right < n - 1 && height[right + 1] < height[left]) {
-                right++;
+            if (left != -1 && right - left > 1) {
+                int level = Math.min(height[left], height[right]);
+                for (int j = left + 1; j < right; j++) volume += Math.max(level - height[j], 0);
+                left = right;
             }
-            right++;
-            // calculate the volume between them:
-            int level = Math.min(height[left], height[right]);
-            for (int i = left + 1; i < right; i++) {
-                volume += level - height[i];
-            }
-            left = right;
         }
 
-        return 0;
+        return volume;
     }
 }
