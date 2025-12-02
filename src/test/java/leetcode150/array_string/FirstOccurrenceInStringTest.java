48731730d6fcb11485092c92f4ed1a5c02d892e5
mstoyanovca <mstoyanovca@gmail.com>
2025-12-01 19:21:01 -0500

    solved FirstOccurrenceInString

---
+++ src/test/java/leetcode150/array_string/FirstOccurrenceInStringTest.java
@@ -0,0 +1,17 @@
+package leetcode150.array_string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstOccurrenceInStringTest {
+    @Test
+    public void example1Test() {
+        assertEquals(0, new FirstOccurrenceInString().strStr("sadbutsad", "sad"));
+    }
+
+    @Test
+    public void example2Test() {
+        assertEquals(-1, new FirstOccurrenceInString().strStr("leetcode", "leeto"));
+    }
+}
