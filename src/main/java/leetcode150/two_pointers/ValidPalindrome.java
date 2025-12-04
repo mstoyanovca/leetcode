2076888696c980379bd9d66fec4a6098d9181213
mstoyanovca <mstoyanovca@gmail.com>
2025-12-03 19:59:59 -0500

    created ValidPalindrome

--- src/main/java/leetcode150/two_pointers/ValidPalindrome.java
+++ src/main/java/leetcode150/two_pointers/ValidPalindrome.java
@@ -1,4 +1,10 @@
 package leetcode150.two_pointers;
 
 public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        String filtered = s.chars().mapToObj(c -> (char) c).filter(c -> Character.isAlphabetic(c) || Character.isDigit(c)).map(Character::toLowerCase).toString();
+        String reversed = new StringBuilder(filtered).reverse().toString();
+
+        return true;
+    }
 }
--- src/test/java/leetcode150/two_pointers/ValidPalindromeTest.java
+++ src/test/java/leetcode150/two_pointers/ValidPalindromeTest.java
@@ -1,4 +1,22 @@
 package leetcode150.two_pointers;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ValidPalindromeTest {
+    @Test
+    public void example1Test() {
+        assertTrue(new ValidPalindrome().isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    @Test
+    public void example2Test() {
+        assertTrue(new ValidPalindrome().isPalindrome("race a car"));
+    }
+
+    @Test
+    public void example3Test() {
+        assertTrue(new ValidPalindrome().isPalindrome(" "));
+    }
 }
