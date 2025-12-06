c839a65393d7a5341d038aa6ce8aa5a92ed716fd
mstoyanovca <mstoyanovca@gmail.com>
2025-12-05 19:41:36 -0500

    solved MinimumSizeSubarraySum

--- src/main/java/leetcode150/two_pointers/MinimumSizeSubarraySum.java
+++ src/main/java/leetcode150/two_pointers/MinimumSizeSubarraySum.java
@@ -5,17 +5,18 @@ public class MinimumSizeSubarraySum {
         int n = nums.length;
         int left = 0;
         int right = 0;
+        int sum = nums[0];
         int result = n + 1;
 
+        // time complexity O(n)
+        // space complexity O(1)
         while (left < n && right < n) {
-            int sum = 0;
-            for (int i = left; i <= right; i++) {
-                sum += nums[i];
-            }
             if (sum >= target) {
                 result = Math.min(result, right - left + 1);
+                if (left < n - 1) sum -= nums[left];
                 left++;
             } else {
+                if (right < n - 1) sum += nums[right + 1];
                 right++;
             }
         }
