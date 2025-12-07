fadade04186923f4f861b0f1151cedc39a53a39a
mstoyanovca <mstoyanovca@gmail.com>
2025-12-07 17:12:18 -0500

    created MinimumWindowSubstring

---
+++ src/main/java/leetcode150/two_pointers/MinimumWindowSubstring.java
@@ -0,0 +1,44 @@
+package leetcode150.two_pointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinimumWindowSubstring {
+    public String minWindow(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int left = 0;
+        int right = 0;
+        int[] minIndexes = new int[2];
+        minIndexes[1] = Integer.MAX_VALUE;
+
+        Map<Character, Integer> tMap = new HashMap<>();
+        for (char c : t.toCharArray()) tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+
+        // time complexity O(?)
+        // space complexity O(?)
+        while (right < n) {
+            char c = s.charAt(right);
+            if (tMap.containsKey(c)) {
+                if (tMap.containsKey(c) & tMap.get(c) > 1) {
+                    tMap.put(c, tMap.get(c) - 1);
+                } else {
+                    tMap.remove(c);
+                }
+            }
+            if (!tMap.isEmpty()) {
+                right++;
+            } else {
+                if (right - left < minIndexes[1] - minIndexes[0]) {
+                    minIndexes[0] = left;
+                    minIndexes[1] = right;
+                }
+                for (char ch : t.toCharArray()) tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
+                left++;
+                right = left;
+            }
+        }
+
+        return minIndexes[1] < Integer.MAX_VALUE ? s.substring(minIndexes[0], minIndexes[1] + 1) : "";
+    }
+}
---
+++ src/test/java/leetcode150/two_pointers/MinimumWindowSubstringTest.java
@@ -0,0 +1,22 @@
+package leetcode150.two_pointers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MinimumWindowSubstringTest {
+    @Test
+    public void example1Test() {
+        assertEquals("BANC", new MinimumWindowSubstring().minWindow("ADOBECODEBANC", "ABC"));
+    }
+
+    @Test
+    public void example2Test() {
+        assertEquals("a", new MinimumWindowSubstring().minWindow("a", "a"));
+    }
+
+    @Test
+    public void example3Test() {
+        assertEquals("", new MinimumWindowSubstring().minWindow("a", "aa"));
+    }
+}
