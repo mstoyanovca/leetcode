c712caec4b8c73ce8d2daa017f8152909dc2faaf
mstoyanovca <mstoyanovca@gmail.com>
2025-12-07 20:03:17 -0500

    solved MinimumWindowSubstring

--- src/main/java/leetcode150/two_pointers/MinimumWindowSubstring.java
+++ src/main/java/leetcode150/two_pointers/MinimumWindowSubstring.java
@@ -6,37 +6,37 @@ import java.util.Map;
 public class MinimumWindowSubstring {
     public String minWindow(String s, String t) {
         int left = 0;
-        int right = 0;
+        int remainingChars = t.length();
         int[] minIndexes = new int[2];
         minIndexes[1] = Integer.MAX_VALUE;
 
         Map<Character, Integer> tMap = new HashMap<>();
         for (char c : t.toCharArray()) tMap.put(c, tMap.getOrDefault(c, 0) + 1);
 
-        // time complexity O(?)
-        // space complexity O(?)
-        while (right < s.length()) {
-            char c = s.charAt(right);
-            if (tMap.containsKey(c)) {
-                if (tMap.containsKey(c) & tMap.get(c) > 1) {
-                    tMap.put(c, tMap.get(c) - 1);
-                } else {
-                    tMap.remove(c);
-                }
+        // time complexity O(n)
+        // space complexity O(m)
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            if (tMap.containsKey(rightChar)) {
+                if (tMap.get(rightChar) > 0) remainingChars--;
+                tMap.put(rightChar, tMap.get(rightChar) - 1);
             }
-            if (!tMap.isEmpty()) {
-                right++;
-            } else {
+
+            while (remainingChars == 0) {
                 if (right - left < minIndexes[1] - minIndexes[0]) {
                     minIndexes[0] = left;
                     minIndexes[1] = right;
                 }
-                for (char ch : t.toCharArray()) tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
+                char leftChar = s.charAt(left);
+                if (tMap.containsKey(leftChar)) {
+                    tMap.put(leftChar, tMap.get(leftChar) + 1);
+                    if (tMap.get(leftChar) > 0) remainingChars++;
+                }
                 left++;
-                right = left;
             }
         }
 
+        // the second argument in substring is exclusive:
         return minIndexes[1] < Integer.MAX_VALUE ? s.substring(minIndexes[0], minIndexes[1] + 1) : "";
     }
 }
