dc1136205c298b8d46ca5739bf15c75e1e96b979
mstoyanovca <mstoyanovca@gmail.com>
2025-12-09 20:02:46 -0500

    solved RotateImage

--- src/main/java/leetcode150/matrix/RotateImage.java
+++ src/main/java/leetcode150/matrix/RotateImage.java
@@ -4,8 +4,10 @@ public class RotateImage {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
+        // time complexity O(n*2)
+        // space complexity O(1)
         for (int r = 0; r < n / 2; r++) {
-            for (int c = 0; c < n - r - 1; c++) {
+            for (int c = r; c < n - r - 1; c++) {
                 int acc = matrix[r][c];
                 matrix[r][c] = matrix[n - c - 1][r];
                 matrix[n - c - 1][r] = matrix[n - r - 1][n - c - 1];
