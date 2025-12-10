6f49a62904339d983f97ef5060dd9f103f694d4f
mstoyanovca <mstoyanovca@gmail.com>
2025-12-09 19:49:51 -0500

    created RotateImage

--- src/main/java/leetcode150/matrix/RotateImage.java
+++ src/main/java/leetcode150/matrix/RotateImage.java
@@ -4,8 +4,8 @@ public class RotateImage {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n - r; c++) {
+        for (int r = 0; r < n / 2; r++) {
+            for (int c = 0; c < n - r - 1; c++) {
                 int acc = matrix[r][c];
                 matrix[r][c] = matrix[n - c - 1][r];
                 matrix[n - c - 1][r] = matrix[n - r - 1][n - c - 1];
