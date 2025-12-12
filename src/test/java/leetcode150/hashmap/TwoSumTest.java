785dd788c11be1a4d7f531b0eee93e8d0172e85e
mstoyanovca <mstoyanovca@gmail.com>
2025-12-11 22:13:17 -0500

    solved TwoSum

---
+++ src/test/java/leetcode150/hashmap/TwoSumTest.java
@@ -0,0 +1,22 @@
+package leetcode150.hashmap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class TwoSumTest {
+    @Test
+    public void example1Test() {
+        assertArrayEquals(new int[]{0, 1}, new TwoSum().twoSum(new int[]{2, 7, 11, 15}, 9));
+    }
+
+    @Test
+    public void example2Test() {
+        assertArrayEquals(new int[]{1, 2}, new TwoSum().twoSum(new int[]{3, 2, 4}, 6));
+    }
+
+    @Test
+    public void example3Test() {
+        assertArrayEquals(new int[]{0, 1}, new TwoSum().twoSum(new int[]{3, 3}, 6));
+    }
+}
