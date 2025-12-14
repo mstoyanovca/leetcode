4c623217a47f09ecb795f10840d06664301fc455
mstoyanovca <mstoyanovca@gmail.com>
2025-12-14 15:46:40 -0500

    solved MinimumNumberOfArrows

--- src/main/java/leetcode150/intervals/MinimumNumberOfArrows.java
+++ src/main/java/leetcode150/intervals/MinimumNumberOfArrows.java
@@ -1,24 +1,21 @@
 package leetcode150.intervals;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class MinimumNumberOfArrows {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, Comparator.comparingInt(a -> a[0]));
-        // List<Integer> arrows = new ArrayList<>();
-        int arrows = 0;
-        int pointer = points[0][1];
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+        int arrows = 1;
+        int arrow = points[0][1];
 
-        for (int i = 0; i < points.length - 1; i++) {
-            if (points[i][1] >= points[i + 1][0] && points[i][1] <= points[i + 1][1]) {
-                int arrow = points[i][1];
-                arrows.add(arrow);
+        for (int i = 1; i < points.length; i++) {
+            if (arrow < points[i][0]) {
+                arrows++;
+                arrow = points[i][1];
             }
         }
 
-        return arrows.size();
+        return arrows;
     }
 }
--- src/test/java/leetcode150/intervals/MinimumNumberOfArrowsTest.java
+++ src/test/java/leetcode150/intervals/MinimumNumberOfArrowsTest.java
@@ -19,4 +19,9 @@ public class MinimumNumberOfArrowsTest {
     public void example3Test() {
         assertEquals(2, new MinimumNumberOfArrows().findMinArrowShots(new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}}));
     }
+
+    @Test
+    public void example4Test() {
+        assertEquals(2, new MinimumNumberOfArrows().findMinArrowShots(new int[][]{{3, 9}, {7, 12}, {3, 8}, {6, 8}, {9, 10}, {2, 9}, {0, 9}, {3, 9}, {0, 6}, {2, 8}}));
+    }
 }
