ad13875919f378e4f1daf7929ce22a2d9aaf02ed
mstoyanovca <mstoyanovca@gmail.com>
2025-12-20 20:59:03 -0500

    created CopyListWithRandomPointer

--- src/main/java/leetcode150/linked_list/CopyListWithRandomPointer.java
+++ src/main/java/leetcode150/linked_list/CopyListWithRandomPointer.java
@@ -1,9 +1,13 @@
 package leetcode150.linked_list;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CopyListWithRandomPointer {
     public Node copyRandomList(Node head) {
         if (head == null) return null;
 
+        Map<Node, Node> oldToNew = new HashMap<>();
         Node result = new Node(head.val);
         Node resultHead = result;
 
@@ -17,9 +21,11 @@ public class CopyListWithRandomPointer {
         temp = head;
         result = resultHead;
         while (temp.next != null) {
-            result.next = new Node(temp.next.val);
-            result = result.next;
-            temp = temp.next;
+            Node next = temp.next;
+            temp.next = result;
+            result.next = next;
+            temp = next;
+            int x = 0;
         }
 
         return resultHead;
