fdb26a5468cee39f5b35df96af37f3f29723b3f5
mstoyanovca <mstoyanovca@gmail.com>
2025-12-21 15:16:39 -0500

    solved ReverseLinkedList2

--- src/main/java/leetcode150/linked_list/ReverseLinkedList2.java
+++ src/main/java/leetcode150/linked_list/ReverseLinkedList2.java
@@ -2,45 +2,26 @@ package leetcode150.linked_list;
 
 public class ReverseLinkedList2 {
     public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode previous = head;
+        ListNode result = head;
         int i = 0;
-        ListNode previous = new ListNode(0);
+
         while (i < left - 1) {
             previous = head;
-            previous.next = null;
             head = head.next;
             i++;
         }
 
-        while (i >= left - 1 && i <= right - 1) {
-            ListNode current = head;
-            head.next.next = head;
-            head.next = null;
-            head = current;
+        while (i >= left - 1 && i < right - 1) {
+            ListNode next = head.next;
+
+            head.next = next.next;
+            next.next = previous.next;
+            previous.next = next;
+
             i++;
         }
 
-        while (i > right - 1) {
-            i++;
-        }
-
-        int x = 0;
-       /* int i = 0;
-        while (head != null) {
-            if (i < left - 1) {
-                continue;
-            } else if (i == left - 1) {
-                before = head;
-            } else if (i == right) {
-
-            } else {
-
-            }
-
-
-            i++;
-            head = head.next;
-        }*/
-
-        return head;
+        return result;
     }
 }
