0fda32a4f123e4bd224bb6c28bb693e2e36cc2fe
mstoyanovca <mstoyanovca@gmail.com>
2025-12-26 14:14:13 -0500

    solved SymmetricTree

--- src/main/java/leetcode150/binary_tree/SymmetricTree.java
+++ src/main/java/leetcode150/binary_tree/SymmetricTree.java
@@ -5,13 +5,12 @@ public class SymmetricTree {
         if (root == null) return true;
         if (root.left == null || root.right == null) return root.left == null && root.right == null;
 
-        boolean result;
-        if (root.left.left != null && root.left.right != null && root.right.left != null && root.right.right != null) {
-            result = root.left.left.val == root.right.right.val && root.left.right.val == root.right.left.val;
-        } else {
-            result = true;
-        }
+        return isTheSame(root.left, root.right);
+    }
 
-        return root.left.val == root.right.val && result && isSymmetric(root.left) && isSymmetric(root.right);
+    private boolean isTheSame(TreeNode p, TreeNode q) {
+        if (p == null || q == null) return p == null && q == null;
+
+        return p.val == q.val && isTheSame(p.left, q.right) && isTheSame(p.right, q.left);
     }
 }
