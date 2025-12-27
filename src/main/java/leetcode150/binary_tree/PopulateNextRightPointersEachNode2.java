770a2a5d188cce7089f461c7aa5538fdf807b840
mstoyanovca <mstoyanovca@gmail.com>
2025-12-27 17:27:44 -0500

    solved PopulateNextRightPointersEachNode2

--- src/main/java/leetcode150/binary_tree/PopulateNextRightPointersEachNode2.java
+++ src/main/java/leetcode150/binary_tree/PopulateNextRightPointersEachNode2.java
@@ -4,11 +4,26 @@ public class PopulateNextRightPointersEachNode2 {
     public Node connect(Node root) {
         if (root == null) return null;
 
-        if (root.left != null && root.right != null) root.left.next = root.right;
-        if (root.left != null && root.left.right != null && root.right != null && root.right.left != null) root.left.right.next = root.right.left;
+        if (root.left != null) {
+            if (root.right != null) {
+                root.left.next = root.right;
+            } else {
+                root.left.next = findNextNodeFromAnotherSubtree(root);
+            }
+        }
+        if (root.right != null) root.right.next = findNextNodeFromAnotherSubtree(root);
 
         root.left = connect(root.left);
         root.right = connect(root.right);
         return root;
+    }
+
+    private Node findNextNodeFromAnotherSubtree(Node root) {
+        while (root.next != null) {
+            if (root.next.left != null) return root.next.left;
+            if (root.next.right != null) return root.next.right;
+        }
+
+        return null;
     }
 }
