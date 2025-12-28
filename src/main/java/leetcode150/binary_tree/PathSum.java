6f4af04e28ec340b43a37fc4e130c82adaf3e0c6
mstoyanovca <mstoyanovca@gmail.com>
2025-12-28 12:59:56 -0500

    solved PathSum

--- src/main/java/leetcode150/binary_tree/PathSum.java
+++ src/main/java/leetcode150/binary_tree/PathSum.java
@@ -3,24 +3,8 @@ package leetcode150.binary_tree;
 public class PathSum {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
-
-        return sum(root, 0, targetSum);
-    }
-
-    private boolean sum(TreeNode root, int acc, int target) {
-        if (root.left == null && root.right == null) {
-            // it is a leaf:
-            acc += root.val;
-            if (acc == target) return true;
-        }
-        if (root.left != null) {
-            acc += root.val;
-            return sum(root.left, acc, target);
-        }
-        if (root.right != null) {
-            acc += root.val;
-            return sum(root.right, acc, target);
-        }
-        return false;
+        // it is a leaf:
+        if (root.left == null && root.right == null) return root.val == targetSum;
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
