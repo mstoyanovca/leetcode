71fe68a46c4a2da1fad013e26276251d37428214
mstoyanovca <mstoyanovca@gmail.com>
2025-12-29 19:17:56 -0500

    solved CountCompleteTreeNodes

---
+++ src/main/java/leetcode150/binary_tree/CountCompleteTreeNodes.java
@@ -0,0 +1,15 @@
+package leetcode150.binary_tree;
+
+public class CountCompleteTreeNodes {
+    private int count = 0;
+
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+
+        countNodes(root.left);
+        count++;
+        countNodes(root.right);
+
+        return count;
+    }
+}
