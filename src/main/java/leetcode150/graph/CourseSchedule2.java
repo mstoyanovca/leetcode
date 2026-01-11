3aba4681af91a344c4529315d75beae8ca3691b1
mstoyanovca <mstoyanovca@gmail.com>
2026-01-10 19:50:33 -0500

    created CourseSchedule2

---
+++ src/main/java/leetcode150/graph/CourseSchedule2.java
@@ -0,0 +1,9 @@
+package leetcode150.graph;
+
+public class CourseSchedule2 {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+
+
+        return null;
+    }
+}
---
+++ src/test/java/leetcode150/graph/CourseSchedule2Test.java
@@ -0,0 +1,22 @@
+package leetcode150.graph;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class CourseSchedule2Test {
+    @Test
+    public void example1Test() {
+        assertArrayEquals(new int[]{0, 1}, new CourseSchedule2().findOrder(2, new int[][]{{1, 0}}));
+    }
+
+    @Test
+    public void example2Test() {
+        assertArrayEquals(new int[]{0, 2, 1, 3}, new CourseSchedule2().findOrder(4, new int[][]{{1, 0}, {2, 0}, {3, 1}, {3, 2}}));
+    }
+
+    @Test
+    public void example3Test() {
+        assertArrayEquals(new int[]{0}, new CourseSchedule2().findOrder(1, new int[][]{}));
+    }
+}
