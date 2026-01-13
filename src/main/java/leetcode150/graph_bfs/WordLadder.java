fe2f67d7e0ef0456317b5c4fbdf2acc294242ff7
mstoyanovca <mstoyanovca@gmail.com>
2026-01-12 20:49:01 -0500

    created WordLadder

---
+++ src/main/java/leetcode150/graph_bfs/WordLadder.java
@@ -0,0 +1,11 @@
+package leetcode150.graph_bfs;
+
+import java.util.List;
+
+public class WordLadder {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+
+        return 0;
+    }
+}
---
+++ src/test/java/leetcode150/graph_bfs/WordLadderTest.java
@@ -0,0 +1,19 @@
+package leetcode150.graph_bfs;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WordLadderTest {
+    @Test
+    public void example1Test() {
+        assertEquals(5, new WordLadder().ladderLength("hit", "cog", List.of("hot", "dot", "dog", "lot", "log", "cog")));
+    }
+
+    @Test
+    public void example2Test() {
+        assertEquals(0, new WordLadder().ladderLength("hit", "cog", List.of("hot", "dot", "dog", "lot", "log")));
+    }
+}
