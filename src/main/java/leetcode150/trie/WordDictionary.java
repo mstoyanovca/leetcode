1688b116cf70fe29d2613281dd305fb2c7d950f8
mstoyanovca <mstoyanovca@gmail.com>
2026-01-14 20:39:14 -0500

    solving WordDictionary

--- src/main/java/leetcode150/trie/WordDictionary.java
+++ src/main/java/leetcode150/trie/WordDictionary.java
@@ -1,15 +1,20 @@
 package leetcode150.trie;
 
-public class WordDictionary {
-    public WordDictionary() {
+import java.util.ArrayList;
+import java.util.List;
 
+public class WordDictionary {
+    private final List<String> list;
+
+    public WordDictionary() {
+        list = new ArrayList<>();
     }
 
     public void addWord(String word) {
-
+        list.add(word);
     }
 
     public boolean search(String word) {
-        return false;
+        return list.contains(word);
     }
 }
--- src/test/java/leetcode150/trie/WordDictionaryTest.java
+++ src/test/java/leetcode150/trie/WordDictionaryTest.java
@@ -2,9 +2,19 @@ package leetcode150.trie;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class WordDictionaryTest {
     @Test
     public void example1Test() {
-
+        WordDictionary wordDictionary = new WordDictionary();
+        wordDictionary.addWord("bad");
+        wordDictionary.addWord("dad");
+        wordDictionary.addWord("mad");
+        assertFalse(new WordDictionary().search("pad"));
+        assertTrue(new WordDictionary().search("bad"));
+        // assertTrue(new WordDictionary().search(".ad"));
+        // assertTrue(new WordDictionary().search("b.."));
     }
 }
