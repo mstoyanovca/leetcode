9cebdd80ddc4fae79a921671122ef970af2fc8b6
mstoyanovca <mstoyanovca@gmail.com>
2026-01-14 23:20:38 -0500

    solving WordDictionary

--- src/main/java/leetcode150/trie/WordDictionary.java
+++ src/main/java/leetcode150/trie/WordDictionary.java
@@ -1,20 +1,28 @@
 package leetcode150.trie;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class WordDictionary {
-    private final List<String> list;
+    private final TrieNode root;
 
     public WordDictionary() {
-        list = new ArrayList<>();
+        root = new TrieNode();
     }
 
     public void addWord(String word) {
-        list.add(word);
+        TrieNode current = root;
+        for (char c : word.toCharArray()) current = current.children.computeIfAbsent(c, _ -> new TrieNode());
+        current.isEndOfWord = true;
     }
 
     public boolean search(String word) {
-        return false;
+        TrieNode current = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == '.') for (char child : current.children.keySet()) return search(word.substring(0, i) + child + word.substring(i + 1));
+            current = current.children.get(c);
+            if (current == null) return false;
+        }
+
+        return current.isEndOfWord;
     }
 }
