48a7cd4e08e96878085a7c84f09e63332c775d81
mstoyanovca <mstoyanovca@gmail.com>
2026-01-17 18:45:06 -0500

    solved PhoneNumberLetterCombinations

---
+++ src/test/java/leetcode150/backtracking/PhoneNumberLetterCombinationsTest.java
@@ -0,0 +1,19 @@
+package leetcode150.backtracking;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PhoneNumberLetterCombinationsTest {
+    @Test
+    public void example1Test() {
+        assertEquals(List.of("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"), new PhoneNumberLetterCombinations().letterCombinations("23"));
+    }
+
+    @Test
+    public void example2Test() {
+        assertEquals(List.of("a", "b", "c"), new PhoneNumberLetterCombinations().letterCombinations("2"));
+    }
+}
