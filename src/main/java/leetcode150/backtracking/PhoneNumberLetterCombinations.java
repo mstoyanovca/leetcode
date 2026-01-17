7ca059576b1c4e35c8dcc31a2f494d6e943e2258
mstoyanovca <mstoyanovca@gmail.com>
2026-01-17 18:54:34 -0500

    solved PhoneNumberLetterCombinations

--- src/main/java/leetcode150/backtracking/PhoneNumberLetterCombinations.java
+++ src/main/java/leetcode150/backtracking/PhoneNumberLetterCombinations.java
@@ -1,12 +1,19 @@
 package leetcode150.backtracking;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PhoneNumberLetterCombinations {
-    private final Map<Character, List<String>> digitToLetters = createDigitToLetters();
+    private final Map<Character, String> digitToLetters = Map.of(
+            '2', "abc",
+            '3', "def",
+            '4', "ghi",
+            '5', "jkl",
+            '6', "mno",
+            '7', "pqrs",
+            '8', "tuv",
+            '9', "wxyz");
 
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
@@ -25,26 +32,11 @@ public class PhoneNumberLetterCombinations {
         // for i = 0 abc, for i = 1 def; it's a nested loop def for each abc;
         // a-d, deletes d, a-e, deletes e, a-f, deletes f, deletes a;
         // starts over with b-d, b-e, b-f , deletes b, c-d, c-e, c-f, deletes c, etc.
-        List<String> letters = digitToLetters.get(digit);  // for i = 0 abc, for i = 1 def
-        for (String letter : letters) {
+        String letters = digitToLetters.get(digit);  // for i = 0 abc, for i = 1 def
+        for (char letter : letters.toCharArray()) {
             builder.append(letter);
             dfs(i + 1, digits, builder, result);
             builder.deleteCharAt(builder.length() - 1);
         }
-    }
-
-    private Map<Character, List<String>> createDigitToLetters() {
-        Map<Character, List<String>> map = new HashMap<>();
-
-        map.put('2', List.of("a", "b", "c"));
-        map.put('3', List.of("d", "e", "f"));
-        map.put('4', List.of("g", "h", "i"));
-        map.put('5', List.of("j", "k", "l"));
-        map.put('6', List.of("m", "n", "o"));
-        map.put('7', List.of("p", "q", "r", "s"));
-        map.put('8', List.of("t", "u", "v"));
-        map.put('9', List.of("w", "x", "y", "z"));
-
-        return map;
     }
 }
