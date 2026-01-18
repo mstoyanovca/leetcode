dd14c02c4a9aac90f838de8706b6a730755d171b
mstoyanovca <mstoyanovca@gmail.com>
2026-01-17 21:21:28 -0500

    solved Combinations

--- src/main/java/leetcode150/backtracking/Combinations.java
+++ src/main/java/leetcode150/backtracking/Combinations.java
@@ -6,15 +6,22 @@ import java.util.List;
 public class Combinations {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
+        backtrack(n, k, 1, new ArrayList<>(), result);
+        return result;
+    }
 
-        for (int left = 1; left <= n - k + 1; left++) {
-            List<Integer> current = new ArrayList<>();
-            for (int i = left; i < left + k; i++) {
-                current.add(i);
-            }
-            result.add(current);
+    private void backtrack(int n, int k, int currentInt, List<Integer> currentList, List<List<Integer>> result) {
+        if (currentList.size() == k) {
+            result.add(new ArrayList<>(currentList));
+            return;
         }
 
-        return result;
+        for (int i = currentInt; i <= n; i++) {
+            if (currentList.isEmpty() || i > currentList.getLast()) {
+                currentList.add(i);
+                backtrack(n, k, currentInt + 1, currentList, result);
+                currentList.removeLast();
+            }
+        }
     }
 }
