429591a12d2ccf658cac7eb3e1443d6e8623b223
mstoyanovca <mstoyanovca@gmail.com>
2026-01-18 20:44:37 -0500

    solving WordSearch

--- src/main/java/leetcode150/backtracking/WordSearch.java
+++ src/main/java/leetcode150/backtracking/WordSearch.java
@@ -4,13 +4,24 @@ public class WordSearch {
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (backtrack(0, 0, board, "", word)) return true;
+                if (backtrack(0, 0, board, "", word)) {
+                    return true;
+                }
             }
         }
+
         return false;
     }
 
     private boolean backtrack(int i, int j, char[][] board, String current, String word) {
-        return false;
+        if (i < 0 || i > board.length || j < 0 || j > board[0].length) return false;
+        if (current.equals(word)) return true;
+
+        boolean left = backtrack(i, j - 1, board, current, word);
+        boolean right = backtrack(i, j + 1, board, current, word);
+        boolean top = backtrack(i - 1, j, board, current, word);
+        boolean bottom = backtrack(i + 1, j, board, current, word);
+
+        return left || right || top || bottom;
     }
 }
