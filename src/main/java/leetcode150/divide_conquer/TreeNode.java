64f298a8a1afe7cfa35e3e8b0e3d1c047bada147
mstoyanovca <mstoyanovca@gmail.com>
2026-01-20 20:53:08 -0500

    solved SortedArrayToBST

--- src/main/java/leetcode150/divide_conquer/TreeNode.java
+++ src/main/java/leetcode150/divide_conquer/TreeNode.java
@@ -1,5 +1,7 @@
 package leetcode150.divide_conquer;
 
+import java.util.Objects;
+
 public class TreeNode {
     int val;
     TreeNode left;
@@ -16,5 +18,16 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof TreeNode treeNode)) return false;
+        return val == treeNode.val && Objects.equals(left, treeNode.left) && Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
     }
 }
