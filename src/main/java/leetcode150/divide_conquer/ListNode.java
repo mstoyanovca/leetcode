135ad857157399e7897c286f152d2f21108ffc68
mstoyanovca <mstoyanovca@gmail.com>
2026-01-21 21:14:22 -0500

    created SortList

---
+++ src/main/java/leetcode150/divide_conquer/ListNode.java
@@ -0,0 +1,31 @@
+package leetcode150.divide_conquer;
+
+import java.util.Objects;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ListNode listNode)) return false;
+        return val == listNode.val && Objects.equals(next, listNode.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next);
+    }
+}
---
+++ src/main/java/leetcode150/divide_conquer/SortList.java
@@ -0,0 +1,51 @@
+package leetcode150.divide_conquer;
+
+public class SortList {
+    // a linked list cannot use random access, merge sort is used
+    // O(n*log(n)) time complexity
+    // O(1) space complexity
+    // keeps the order of equal elements
+    // LinkedList friendly: merge sort does not require random access
+    // divide: use slow and fast pointer to find the middle of the linked list
+    // conquer: recursively sort both halves
+    // combine: merge the sorted halves into one sorted list
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        // find the middle:
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // split:
+        ListNode middle = slow.next;
+        slow.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(middle);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode listNode1, ListNode listNode2) {
+        ListNode current = new ListNode(0);
+        ListNode tail = current;
+
+        while (listNode1 != null && listNode2 != null) {
+            if (listNode1.val < listNode2.val) {
+                tail.next = listNode1;
+                listNode1 = listNode1.next;
+            } else {
+                tail.next = listNode2;
+                listNode2 = listNode2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = (listNode1 != null) ? listNode1 : listNode2;
+        return current.next;
+    }
+}
