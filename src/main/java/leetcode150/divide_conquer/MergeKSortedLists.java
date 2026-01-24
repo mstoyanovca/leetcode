55f75bf81832abb6279e4981b5b9cc6a0232a91d
mstoyanovca <mstoyanovca@gmail.com>
2026-01-23 19:16:38 -0500

    created MergeKSortedLists

---
+++ src/main/java/leetcode150/divide_conquer/MergeKSortedLists.java
@@ -0,0 +1,12 @@
+package leetcode150.divide_conquer;
+
+public class MergeKSortedLists {
+    // divide: split the k lists into pairs
+    // conquer: merge two sorted lists at a time
+    // repeat
+    // time complexity O(N*log(k)), where N is the number of nodes across all lists
+    // space complexity: O(log(k)) due to the recursive function calls
+    public ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+}
