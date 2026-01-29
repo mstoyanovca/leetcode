577163588a72f718713e3db005765d37edebdb1a
mstoyanovca <mstoyanovca@gmail.com>
2026-01-28 21:40:42 -0500

    solved FindKthElementInTwoSortedArrays

--- src/main/java/dsa_tutorial/array/FindKthElementInTwoSortedArrays.java
+++ src/main/java/dsa_tutorial/array/FindKthElementInTwoSortedArrays.java
@@ -1,34 +1,39 @@
 package dsa_tutorial.array;
 
 public class FindKthElementInTwoSortedArrays {
+    // time complexity O(log(min(m, n)))
+    // space complexity O(1)
     public int findKthElement(int[] a, int[] b, int k) {
-        // start splitting the shorter array to achieve time complexity O(log(min(m, n)))
-        // space complexity O(1)
         if (a.length > b.length) return findKthElement(b, a, k);
+
         int m = a.length;
         int n = b.length;
-        int left = Math.max(0, k - n);
-        int right = Math.min(k, m);
+        // number of elements from 'a' can range from max(0, k - m) to min(k, n):
+        int min = Math.max(0, k - n);  // 0
+        int max = Math.min(k, m);      // 4
 
-        while (left <= right) {
-            int i = (left + right) / 2;
-            int j = k - i;
+        while (min <= max) {
+            int m1 = (min + max) / 2;  // 2 elements from a
+            int m2 = k - m1;           // 3 elements from b
 
-            int leftA = (i == 0) ? Integer.MIN_VALUE : a[i - 1];
-            int rightA = (i == m) ? Integer.MAX_VALUE : a[i];
+            // the four boundary elements for both partitions:
+            int l1 = m1 > 0 ? a[m1 - 1] : Integer.MIN_VALUE;  // 1
+            int r1 = m1 < m ? a[m1] : Integer.MAX_VALUE;      // 2
+            int l2 = m2 > 0 ? b[m2 - 1] : Integer.MIN_VALUE;  // 2
+            int r2 = m2 < n ? b[m2] : Integer.MAX_VALUE;      // 2
 
-            int leftB = (j == 0) ? Integer.MIN_VALUE : b[j - 1];
-            int rightB = (j == n) ? Integer.MAX_VALUE : b[j];
-
-            if (leftA <= rightB && leftB <= rightA) {
-                return Math.max(leftA, leftB);
-            } else if (leftA > rightB) {
-                right = i - 1;
+            if (l1 <= r2 && l2 <= r1) {
+                // a valid partition:
+                return Math.max(l1, l2);
+            } else if (l1 > r2) {
+                // take less from a:
+                max = m1 - 1;
             } else {
-                left = i + 1;
+                // take more from a:
+                min = m1 + 1;
             }
         }
 
-        return 0;
+        return -1;
     }
 }
