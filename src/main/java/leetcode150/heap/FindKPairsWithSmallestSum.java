27571cc969c713f57d48e9d907131b0441eac050
mstoyanovca <mstoyanovca@gmail.com>
2026-01-31 19:26:09 -0500

    solved FindKPairsWithSmallestSum

--- src/main/java/leetcode150/heap/FindKPairsWithSmallestSum.java
+++ src/main/java/leetcode150/heap/FindKPairsWithSmallestSum.java
@@ -1,27 +1,38 @@
 package leetcode150.heap;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class FindKPairsWithSmallestSum {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        Queue<List<Integer>> minHeap = new PriorityQueue<>((a, b) -> a.get(0) + a.get(1) - b.get(0) - b.get(1));
+        Queue<List<Integer>> minHeap = new PriorityQueue<>(Comparator.comparingInt(List::getFirst));
         List<List<Integer>> result = new ArrayList<>();
 
-        /*for (int i = 0; i < nums1.length; i++) {
-            List<Integer> current = new ArrayList<>();
-            current.add()
-            minHeap.add(new ArrayList<>())
-        }*/
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < k; j++) {
-                minHeap.add(List.of(nums1[i], nums2[j]));
-            }
+        for (int i = 0; i < k && i < nums1.length; i++) {
+            List<Integer> currentSum = new ArrayList<>();
+            currentSum.add(nums1[i] + nums2[0]);
+            currentSum.add(0);
+
+            minHeap.add(currentSum);
         }
 
-        for (int i = 0; i < k; i++) result.add(minHeap.remove());
+        while (k > 0 && !minHeap.isEmpty()) {
+            List<Integer> currentSum = minHeap.remove();
+            int sum = currentSum.get(0);
+            int nums2Index = currentSum.get(1);
+            int nums1Value = sum - nums2[nums2Index];
+
+            List<Integer> currentPair = new ArrayList<>();
+            currentPair.add(nums1Value);
+            currentPair.add(nums2[nums2Index]);
+            result.add(currentPair);
+
+            if (nums2Index + 1 < nums2.length) {
+                int newSum = sum - nums2[nums2Index] + nums2[nums2Index + 1];
+                minHeap.add(List.of(newSum, nums2Index + 1));
+            }
+
+            k--;
+        }
 
         return result;
     }
