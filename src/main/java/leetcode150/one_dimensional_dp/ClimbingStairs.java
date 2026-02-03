8bc0dd69b91a5c1886f717c7c9ebcd0bee9bf985
mstoyanovca <mstoyanovca@gmail.com>
2026-02-02 21:59:08 -0500

    created ClimbingStairs

--- src/main/java/leetcode150/one_dimensional_dp/ClimbingStairs.java
+++ src/main/java/leetcode150/one_dimensional_dp/ClimbingStairs.java
@@ -2,6 +2,13 @@ package leetcode150.one_dimensional_dp;
 
 public class ClimbingStairs {
     public int climbStairs(int n) {
-        return n;
+        if (n == 0 || n == 1) return 1;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    // time complexity O(2^n), it times out
+    public int climbStairsRecursive(int n) {
+        if (n == 0 || n == 1) return 1;
+        return climbStairsRecursive(n - 1) + climbStairsRecursive(n - 2);
     }
 }
