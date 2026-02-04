8ed1b4eebe99775b09bf26578ddb551eed1e8310
mstoyanovca <mstoyanovca@gmail.com>
2026-02-03 21:34:18 -0500

    solving WordBreak

--- src/main/java/leetcode150/one_dimensional_dp/WordBreak.java
+++ src/main/java/leetcode150/one_dimensional_dp/WordBreak.java
@@ -1,24 +1,25 @@
 package leetcode150.one_dimensional_dp;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
-        int n = s.length();
-        boolean[] dp = new boolean[n];
-        for (int left = 0; left < n; left++) {
-            for (int right = left; right < n; right++) {
-                String ss = s.substring(left, right + 1);
-                if (wordDict.contains(ss))
-                    dp[right] = true;
+        return wordBreakUtility(s, wordDict, new HashMap<>());
+    }
+
+    public boolean wordBreakUtility(String s, List<String> wordDict, Map<String, Boolean> map) {
+        if (s.isEmpty()) return true;
+        //if (map.containsKey(s)) return map.get(s);
+
+        for (int left = 0; left < s.length(); left++) {
+            String substring = s.substring(0, left + 1);
+            if (wordDict.contains(substring)) {
+                return wordBreakUtility(s.substring(left + 1), wordDict, map);
             }
         }
 
-        return dp[n - 1];
-    }
-
-    public boolean wordBreak2(String s, List<String> wordDict) {
-        for (String word : wordDict) if (s.contains(word)) s = s.replace(word, "");
-        return s.isEmpty();
+        return false;
     }
 }
