2229804d8994be3ea694fde172ae11b8896e887a
mstoyanovca <mstoyanovca@gmail.com>
2026-02-03 19:37:01 -0500

    created HouseRobber

--- src/main/java/leetcode150/one_dimensional_dp/HouseRobber.java
+++ src/main/java/leetcode150/one_dimensional_dp/HouseRobber.java
@@ -2,18 +2,12 @@ package leetcode150.one_dimensional_dp;
 
 public class HouseRobber {
     public int rob(int[] nums) {
-        int max1 = 0;
-        int max2 = 0;
+        return robUtility(nums, 0);
+    }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i % 2 == 0) {
-                max1 += nums[i];
-            } else {
-                max2 += nums[i];
-            }
-
-        }
-
-        return Math.max(max1, max2);
+    // this works, but it times out, because it processes the same 'i' multiple times, memorization is needed:
+    private int robUtility(int[] nums, int i) {
+        if (i >= nums.length) return 0;
+        return Math.max((nums[i] + robUtility(nums, i + 2)), robUtility(nums, i + 1));
     }
 }
