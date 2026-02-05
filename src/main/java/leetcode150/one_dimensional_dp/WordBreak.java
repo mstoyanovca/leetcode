3385334259c745b671c76ab4123cab62e9c28e15
mstoyanovca <mstoyanovca@gmail.com>
2026-02-04 21:10:16 -0500

    solving WordBreak

--- src/main/java/leetcode150/one_dimensional_dp/WordBreak.java
+++ src/main/java/leetcode150/one_dimensional_dp/WordBreak.java
@@ -1,22 +1,26 @@
 package leetcode150.one_dimensional_dp;
 
-import java.util.List;
+import java.util.*;
 
 public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return wordBreakUtility(s, wordDict);
+        // time complexity of contains() is O(1) for HashSet and O(n) for List:
+        return wordBreakUtility(s, new HashSet<>(wordDict), new HashMap<>(), s.length());
     }
 
     // this works, but it times out, it needs a map:
-    public boolean wordBreakUtility(String s, List<String> wordDict) {
+    public boolean wordBreakUtility(String s, Set<String> dictionary, Map<Integer, String> memo, int length) {
         if (s.isEmpty()) return true;
+        if (memo.containsKey(length - 1)) return true;
 
         for (int left = 0; left < s.length(); left++) {
-            if (wordDict.contains(s.substring(0, left + 1)) && wordBreakUtility(s.substring(left + 1), wordDict)) {
+            String substring = s.substring(0, left + 1);
+            if (dictionary.contains(substring) && wordBreakUtility(s.substring(left + 1), dictionary, memo, length)) {
+                memo.put(left, substring);
                 return true;
             }
         }
 
-        return false;
+        return memo.containsKey(length - 1);
     }
 }
