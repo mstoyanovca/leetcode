728e240ebe67cc081c50f081797f790802d0d9db
mstoyanovca <mstoyanovca@gmail.com>
2026-02-05 22:06:11 -0500

    solving CoinChange

--- src/main/java/leetcode150/one_dimensional_dp/CoinChange.java
+++ src/main/java/leetcode150/one_dimensional_dp/CoinChange.java
@@ -4,21 +4,47 @@ import java.util.Arrays;
 
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) return 0;
-        Arrays.sort(coins);
-        // int[] sorted = IntStream.of(coins).boxed().sorted(Comparator.reverseOrder()).mapToInt(i -> i).toArray();
-        return coinChangeUtility(coins, coins.length - 1, amount, -1);
+        int ans = coinChangeUtility(coins, amount);
+        return (ans == Integer.MAX_VALUE) ? -1 : ans;
     }
 
-    private int coinChangeUtility(int[] coins, int index, int amount, int result) {
-        if (amount == 0) return result;
-        if (index < 0) return -1;
+    // recursion (top-down approach)
+    // time complexity O(n ^ amount)
+    // space complexity O(amount)
+    public int coinChangeUtility(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        if (amount < 0) return Integer.MAX_VALUE;
+        int minCoins = Integer.MAX_VALUE;
 
-        while (amount >= coins[index]) {
-            amount -= coins[index];
-            result = result == -1 ? 1 : result + 1;
+        for (int i = 0; i < coins.length; i++) {
+            int ans = coinChangeUtility(coins, amount - coins[i]);
+
+            if (ans != Integer.MAX_VALUE) {
+                //we have returned 0 in ans, so now we are updating the ans count
+                //hence 1 + ans
+                minCoins = Math.min(minCoins, 1 + ans);
+            }
         }
 
-        return coinChangeUtility(coins, --index, amount, result);
+        return minCoins;
+    }
+
+    // a classic bottom-up DP problem
+    // time complexity O(amount * n)
+    // space complexity O(amount)
+    public int coinChangeBottomUp(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
