0130e4687b6e5bb75dab50dd7596d865c42241a8
mstoyanovca <mstoyanovca@gmail.com>
2026-02-07 17:56:46 -0500

    solved LongestIncreasingSubsequence

--- src/main/java/leetcode150/one_dimensional_dp/LongestIncreaisngSubsequence.java
+++ src/main/java/leetcode150/one_dimensional_dp/LongestIncreaisngSubsequence.java
@@ -2,12 +2,27 @@ package leetcode150.one_dimensional_dp;
 
 public class LongestIncreaisngSubsequence {
     public int lengthOfLIS(int[] nums) {
-        if (nums.length == 1) return 1;
+        int[] subsequence = new int[nums.length];
+        int size = 0;
 
-        return lengthOfLISUtility(nums);
+        for (int num : nums) {
+            int left = 0;
+            int right = size;
+
+            while (left != right) {
+                int middle = (left + right) / 2;
+                if (subsequence[middle] < num) left = middle + 1;
+                else right = middle;
+            }
+
+            subsequence[left] = num;
+            if (left == size) ++size;
+        }
+
+        return size;
     }
 
-    public int lengthOfLISUtility(int[] nums) {
+    public int lengthOfLISUtility2(int[] nums) {
         int maxResult = 0;
 
         for (int i = 0; i < nums.length; i++) {
