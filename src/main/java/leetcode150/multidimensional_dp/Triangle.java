2d1ede1f6ac2a66f4f397c9932d67b2e6b4bbdaa
mstoyanovca <mstoyanovca@gmail.com>
2026-02-07 19:58:35 -0500

    solved Triangle

--- src/main/java/leetcode150/multidimensional_dp/Triangle.java
+++ src/main/java/leetcode150/multidimensional_dp/Triangle.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 public class Triangle {
     // bottom-up DP approach: iterate from the second last row (n-2) up to the top (row 0)
+    // for each cell (i, j), compute triangle[i][j] + min(triangle[i+1][j], triangle[i+1][j+1])
+    // triangle[0][0] will contain the minimum path sum
+    // update the triangle in-place
     // time complexity O(n ^ 2)
     // space complexity O(1)
     public int minimumTotal(List<List<Integer>> triangle) {
         for (int i = triangle.size() - 2; i >= 0; i--) {
-            for (int j = 0; j < triangle.get(i).size() - 1; j++) {
+            // for (int j = 0; j <= triangle.get(i).size() - 1; j++) {
+            for (int j = 0; j <= i; j++) {
                 int temp = triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1));
                 triangle.get(i).set(j, temp);
             }
