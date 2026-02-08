9e1084ce538417d808fc9730e191831524cfd218
mstoyanovca <mstoyanovca@gmail.com>
2026-02-08 16:51:48 -0500

    solved EditDistance

---
+++ src/main/java/leetcode150/multidimensional_dp/EditDistance.java
@@ -0,0 +1,22 @@
+package leetcode150.multidimensional_dp;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class EditDistance {
+    public int minDistance(String word1, String word2) {
+        final int m = word1.length();
+        final int n = word2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m + 1; i++) dp[i][0] = i;
+        for (int j = 0; j < n + 1; j++) dp[0][j] = j;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = Collections.min(Arrays.asList(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])) + 1;
+
+        return dp[m][n];
+    }
+}
