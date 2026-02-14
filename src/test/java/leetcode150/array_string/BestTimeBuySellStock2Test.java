0b51500d1302645844c66d1d35de6aff487604df
mstoyanovca <mstoyanovca@gmail.com>
2026-02-14 17:01:07 -0500

    lc150 second pass

--- src/test/java/leetcode150/array_string/BestTimeBuySellStock2Test.java
+++ src/test/java/leetcode150/array_string/BestTimeBuySellStock2Test.java
@@ -9,4 +9,14 @@ public class BestTimeBuySellStock2Test {
     public void example1Test() {
         assertEquals(7, new BestTimeBuySellStock2().maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
+
+    @Test
+    public void example2Test() {
+        assertEquals(4, new BestTimeBuySellStock2().maxProfit(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    public void example3Test() {
+        assertEquals(0, new BestTimeBuySellStock2().maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
 }
