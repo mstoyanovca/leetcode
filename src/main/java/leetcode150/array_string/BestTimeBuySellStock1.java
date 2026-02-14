3fc51c3a8fe8b3fe7cf3d0b75acb8ab997656e1f
mstoyanovca <mstoyanovca@gmail.com>
2026-02-13 21:00:12 -0500

    lc150 second pass

--- src/main/java/leetcode150/array_string/BestTimeBuySellStock1.java
+++ src/main/java/leetcode150/array_string/BestTimeBuySellStock1.java
@@ -3,35 +3,20 @@ package leetcode150.array_string;
 public class BestTimeBuySellStock1 {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int left = 0;
-        int right = 0;
-        int maxProfit = 0;
+        int buy = prices[0];
+        int sell = prices[0];
+        int profit = 0;
 
         // time complexity O(n)
         // space complexity O(1)
-        // find a global extremum:
-        while (right < n) {
-            // slide the left pointer downhill to the next local minimum:
-            while (left + 1 < n && prices[left + 1] < prices[left]) {
-                left++;
-                // can't sell before buy:
-                right = left;
-            }
-            // slide the right pointer uphill to the next local maximum:
-            while (right + 1 < n && prices[right + 1] > prices[right]) {
-                right++;
-            }
-            maxProfit = Math.max(prices[right] - prices[left], maxProfit);
-            if (right + 1 < n) {
-                // move the right pointer downhill, expecting new local maximum after that:
-                right++;
-                // it might be a global minimum:
-                if (prices[right] < prices[left]) left = right;
-            } else {
-                break;
+        for (int price : prices) {
+            if (price < buy) {
+                buy = price;
+            } else if (price - buy > profit) {
+                profit = price - buy;
             }
         }
 
-        return maxProfit;
+        return profit;
     }
 }
