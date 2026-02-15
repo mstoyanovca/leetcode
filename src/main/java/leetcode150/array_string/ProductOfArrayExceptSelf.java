88420c516478317db0c7f79af87aa272c8ad2bbd
mstoyanovca <mstoyanovca@gmail.com>
2026-02-15 12:28:19 -0500

    lc150 second pass

--- src/main/java/leetcode150/array_string/ProductOfArrayExceptSelf.java
+++ src/main/java/leetcode150/array_string/ProductOfArrayExceptSelf.java
@@ -8,33 +8,21 @@ public class ProductOfArrayExceptSelf {
         // time complexity O(n)
         // space complexity O(1)
         // use the result as an in-place suffix array:
-        for (int i = n - 1; i >= 0; i--) {
-            if (i == n - 1) {
-                result[i] = 1;
-            } else if (i == n - 2) {
-                result[i] = nums[i + 1];
-            } else {
-                result[i] = result[i + 1] * nums[i + 1];
-            }
-        }
+        result[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--)
+            result[i] = result[i + 1] * nums[i + 1];
         // use the input as an in-place prefix array:
         int acc = nums[0];
         for (int i = 0; i < n; i++) {
             if (i == 0) {
                 nums[i] = 1;
-            } else if (i == 1) {
-                int temp = nums[i];
-                nums[i] = nums[i - 1] * acc;
-                acc = temp;
             } else {
                 int temp = nums[i];
                 nums[i] = nums[i - 1] * acc;
                 acc = temp;
             }
-        }
-        // each result[i] = prefix[i] * suffix[i]:
-        // calculate suffix first, before you overwrite the input array :)
-        for (int i = 0; i < n; i++) {
+            // each result[i] = prefix[i] * suffix[i];
+            // calculate suffix first, before you overwrite the input array :)
             result[i] *= nums[i];
         }
 
