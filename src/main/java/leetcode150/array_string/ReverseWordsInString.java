cac6af7c48d204a17a284e2b5cd3a750e351b2c1
mstoyanovca <mstoyanovca@gmail.com>
2026-02-16 14:06:09 -0500

    cleaning

--- src/main/java/leetcode150/array_string/ReverseWordsInString.java
+++ src/main/java/leetcode150/array_string/ReverseWordsInString.java
@@ -5,6 +5,9 @@ import java.util.function.Predicate;
 
 public class ReverseWordsInString {
     public String reverseWords(String s) {
+        // I assume:
+        // time complexity O(n)
+        // space complexity O(1)
         return String.join(" ", Arrays.stream(s.trim().split(" ")).map(String::trim).filter(Predicate.not(String::isBlank)).toList().reversed());
     }
 }
