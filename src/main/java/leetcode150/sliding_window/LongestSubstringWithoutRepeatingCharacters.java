05d10529454dc1c5700ff1ec81bf98e537fc1d62
mstoyanovca <mstoyanovca@gmail.com>
2026-02-16 20:00:01 -0500

    second pass

--- src/main/java/leetcode150/sliding_window/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/leetcode150/sliding_window/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,25 +1,26 @@
 package leetcode150.sliding_window;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
         int left = 0;
         int right = 0;
-        Map<Character, Integer> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
         int result = 0;
 
         // I assume:
-        // time complexity O(n)
-        // space complexity O(n)
-        while (right < s.length()) {
-            if (!map.containsKey(s.charAt(right))) {
-                map.put(s.charAt(right), 1);
-                result = Math.max(result, map.size());
+        // time complexity O(m)
+        // space complexity O(m)
+        while (right < n) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                result = Math.max(result, set.size());
                 right++;
             } else {
-                map.remove(s.charAt(left));
+                set.remove(s.charAt(left));
                 left++;
             }
         }
