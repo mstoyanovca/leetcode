e48103834911b4625ebeee51b9b4dcca24147f05
mstoyanovca <mstoyanovca@gmail.com>
2026-02-19 19:54:51 -0500

    second pass

--- src/main/java/leet_code_75/array_string/ReverseWordsInString.java
+++ src/main/java/leet_code_75/array_string/ReverseWordsInString.java
@@ -1,20 +1,14 @@
 package leet_code_75.array_string;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.function.Predicate;
 
 public class ReverseWordsInString {
     public String reverseWords(String s) {
-        List<String> input = new ArrayList<>(Arrays.stream(s.split(" ")).map(String::trim).filter(e -> !e.isEmpty()).toList());
-        Collections.reverse(input);
-
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < input.size(); i++) {
-            builder.append(input.get(i));
-            if (i < input.size() - 1) builder.append(" ");
-        }
-        return builder.toString();
+        // I assume:
+        // time complexity O(n)
+        // space complexity O(1)
+        return String.join(" ", Arrays.stream(s.trim().split(" ")).map(String::trim).filter(s1 -> !s1.isBlank())
+                .filter(Predicate.not(String::isBlank)).toList().reversed());
     }
 }
