1fd9e06439bdbfe5c78873501cace920847e2ac6
mstoyanovca <mstoyanovca@gmail.com>
2026-02-20 19:15:22 -0500

    second pass

--- src/main/java/top_interview_150/array_string/HIndex.java
+++ src/main/java/top_interview_150/array_string/HIndex.java
@@ -7,10 +7,10 @@ public class HIndex {
         int n = citations.length;
         int hIndex = 0;
 
-        // time complexity O(n)
-        // space complexity O(1)
         Arrays.sort(citations);
 
+        // time complexity O(n)
+        // space complexity O(1)
         for (int i = 0; i < n; i++) hIndex = Math.max(hIndex, Math.min(citations[i], n - i));
 
         return hIndex;
