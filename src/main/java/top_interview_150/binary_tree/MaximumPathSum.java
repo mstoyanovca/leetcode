10c0860de3e820d8d9d0b5fa82d9b585a6f15de3
mstoyanovca <mstoyanovca@gmail.com>
2026-02-22 17:53:59 -0500

    second pass

--- src/main/java/top_interview_150/binary_tree/MaximumPathSum.java
+++ src/main/java/top_interview_150/binary_tree/MaximumPathSum.java
@@ -4,15 +4,17 @@ public class MaximumPathSum {
     private int result = Integer.MIN_VALUE;
 
     int maxPathSum(TreeNode root) {
-        maxPathSumUtil(root);
+        maxPathSumUtility(root);
         return result;
     }
 
-    private int maxPathSumUtil(TreeNode root) {
+    private int maxPathSumUtility(TreeNode root) {
         if (root == null) return 0;
 
-        int left = Math.max(maxPathSumUtil(root.left), 0);
-        int right = Math.max(maxPathSumUtil(root.right), 0);
+        // if the left leaf is negative, and brings the overall sum down, ignore it:
+        int left = Math.max(0, maxPathSumUtility(root.left));
+        // if the right leaf is negative, and brings the overall sum down, ignore it:
+        int right = Math.max(0, maxPathSumUtility(root.right));
 
         result = Math.max(result, root.val + left + right);
 
