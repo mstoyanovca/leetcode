c451f5654655bbd6fc7e84dac3ff10b2f3fbd1ad
mstoyanovca <mstoyanovca@gmail.com>
2026-02-22 20:16:31 -0500

    second pass

--- src/main/java/top_interview_150/bst/MinimumAbsoluteDifference.java
+++ src/main/java/top_interview_150/bst/MinimumAbsoluteDifference.java
@@ -1,18 +1,50 @@
 package top_interview_150.bst;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MinimumAbsoluteDifference {
+    TreeNode previous = null;
+    TreeNode current = null;
     int result = Integer.MAX_VALUE;
-    int previous = -1;
-    int current = -1;
 
-
+    // I assume:
+    // time complexity O(n)
+    // space complexity O(n)
+    // this solution provides space complexity O(n), but it's more intuitive;
+    // this problem asks for the inorder list of the tree;
     int getMinimumDifference(TreeNode root) {
+        List<TreeNode> inorder = new ArrayList<>();
+        buildInorderList(root, inorder);
+        for (int i = 1; i < inorder.size(); i++) result = Math.min(result, inorder.get(i).val - inorder.get(i - 1).val);
+
+        return result;
+    }
+
+    // inorder = left - root - right
+    private void buildInorderList(TreeNode root, List<TreeNode> inorder) {
+        if (root == null) {
+            return;
+        }
+
+        buildInorderList(root.left, inorder);
+        inorder.add(root);
+        buildInorderList(root.right, inorder);
+    }
+
+
+    // I assume:
+    // time complexity O(n)
+    // space complexity O(1)
+    int getMinimumDifference2(TreeNode root) {
         if (root == null) return 0;
 
         getMinimumDifference(root.left);
+
         previous = current;
-        current = root.val;
-        if (previous != -1 && current != -1) result = Math.min(result, current - previous);
+        current = root;
+        if (previous != null) result = Math.min(result, current.val - previous.val);
+
         getMinimumDifference(root.right);
 
         return result;
