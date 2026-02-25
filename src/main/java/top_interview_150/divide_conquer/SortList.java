fe3574a73dba6723d76363b2ac4d1793d86f67fd
mstoyanovca <mstoyanovca@gmail.com>
2026-02-24 22:39:19 -0500

    second pass

--- src/main/java/top_interview_150/divide_conquer/SortList.java
+++ src/main/java/top_interview_150/divide_conquer/SortList.java
@@ -2,20 +2,17 @@ package top_interview_150.divide_conquer;
 
 public class SortList {
     // merge sort algorithm, uses the divide-and-conquer approach
-    // O(n*log(n)) time complexity
+    // O(n * log(n)) time complexity
     // O(1) space complexity
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) return head;
 
         ListNode slow = head;
-        ListNode fast = head.next;
-        // find the middle:
-        while (fast != null && fast.next != null) {
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-
-        // split:
         ListNode middle = slow.next;
         slow.next = null;
 
@@ -25,22 +22,22 @@ public class SortList {
         return merge(left, right);
     }
 
-    private ListNode merge(ListNode listNode1, ListNode listNode2) {
-        ListNode current = new ListNode(0);
-        ListNode tail = current;
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode result = new ListNode();
+        ListNode current = result;
 
-        while (listNode1 != null && listNode2 != null) {
-            if (listNode1.val < listNode2.val) {
-                tail.next = listNode1;
-                listNode1 = listNode1.next;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                current.next = left;
+                left = left.next;
             } else {
-                tail.next = listNode2;
-                listNode2 = listNode2.next;
+                current.next = right;
+                right = right.next;
             }
-            tail = tail.next;
+            current = current.next;
         }
+        current.next = left != null ? left : right;
 
-        tail.next = (listNode1 != null) ? listNode1 : listNode2;
-        return current.next;
+        return result.next;
     }
 }
