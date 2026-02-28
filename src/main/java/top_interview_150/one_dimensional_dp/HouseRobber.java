fdbc93a7ab0654f232893e2ca863cb2553a645b4
mstoyanovca <mstoyanovca@gmail.com>
2026-02-28 12:39:31 -0500

    second pass

--- src/main/java/top_interview_150/one_dimensional_dp/HouseRobber.java
+++ src/main/java/top_interview_150/one_dimensional_dp/HouseRobber.java
@@ -5,20 +5,20 @@ import java.util.Map;
 
 public class HouseRobber {
     public int rob(int[] nums) {
-        return robUtility(nums, 0, new HashMap<>());
+        return robUtility(0, nums, new HashMap<>());
     }
 
     // time complexity O(n)
     // space complexity O(n)
-    private int robUtility(int[] nums, int i, Map<Integer, Integer> map) {
+    private int robUtility(int i, int[] nums, Map<Integer, Integer> memo) {
         if (i >= nums.length) return 0;
-        if (!map.containsKey(i)) map.put(i, Math.max((nums[i] + robUtility(nums, i + 2, map)), robUtility(nums, i + 1, map)));
-        return map.get(i);
+        if (!memo.containsKey(i)) memo.put(i, Math.max((nums[i] + robUtility(i + 2, nums, memo)), robUtility(i + 1, nums, memo)));
+        return memo.get(i);
     }
 
     // this works, but it times out, because it processes the same 'i' multiple times, memorization is needed:
-    private int robUtility2(int[] nums, int i) {
+    private int robUtilityRecursive(int i, int[] nums) {
         if (i >= nums.length) return 0;
-        return Math.max((nums[i] + robUtility2(nums, i + 2)), robUtility2(nums, i + 1));
+        return Math.max((nums[i] + robUtilityRecursive(i + 2, nums)), robUtilityRecursive(i + 1, nums));
     }
 }
