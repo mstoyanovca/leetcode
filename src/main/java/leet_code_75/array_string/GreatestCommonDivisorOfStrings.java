ae2a38a179fe1a39f30147a6129be64f4c7d86d9
mstoyanovca <mstoyanovca@gmail.com>
2026-09-21 19:40:52 -0400

    refactored GreatestCommonDivisorOfStrings

--- src/main/java/leet_code_75/array_string/GreatestCommonDivisorOfStrings.java
+++ src/main/java/leet_code_75/array_string/GreatestCommonDivisorOfStrings.java
@@ -1,38 +1,15 @@
 package leet_code_75.array_string;
 
 public class GreatestCommonDivisorOfStrings {
-    public String gcdOfStrings(String string1, String string2) {
-        String longerString;
-        String shorterString;
-        if (string1.length() > string2.length()) {
-            longerString = string1;
-            shorterString = string2;
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
         } else {
-            longerString = string2;
-            shorterString = string1;
+            return str1.substring(0, gcd(str1.length(), str2.length()));
         }
-
-        StringBuilder builder = new StringBuilder(shorterString);
-        if (isDivisor(longerString, builder.toString())) {
-            return builder.toString();
-        } else {
-            builder.deleteCharAt(builder.length() - 1);
-        }
-
-        while (!builder.isEmpty() && (!isDivisor(longerString, builder.toString()) || !isDivisor(shorterString, builder.toString()))) {
-            builder.deleteCharAt(builder.length() - 1);
-        }
-
-        return builder.toString();
     }
 
-    private boolean isDivisor(String dividend, String divisor) {
-        if (dividend.length() % divisor.length() == 0) {
-            StringBuilder builder = new StringBuilder();
-            builder.append(divisor.repeat(dividend.length() / divisor.length()));
-            return dividend.contentEquals(builder);
-        } else {
-            return false;
-        }
+    private static int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
     }
 }
