9d708c8899668d3cb4d5e414e2711ed628dc2e81
mstoyanovca <mstoyanovca@gmail.com>
2026-09-21 21:39:12 -0400

    refactored ReverseVowelsOfString

--- src/main/java/leet_code_75/array_string/ReverseVowelsOfString.java
+++ src/main/java/leet_code_75/array_string/ReverseVowelsOfString.java
@@ -1,42 +1,30 @@
 package leet_code_75.array_string;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 public class ReverseVowelsOfString {
-    private final char[] allVowels = {'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'};
+    private final Set<Character> vowels = Set.of('A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u');
 
     public String reverseVowels(String s) {
-        char[] input = s.toCharArray();
+        StringBuilder builder = new StringBuilder(s);
+        int left = 0;
+        int right = s.length() - 1;
 
-        List<Character> vowelsInInput = new ArrayList<>();
-        int[] vowelsMap = new int[s.length()];
-        for (int i = 0; i < input.length; i++) {
-            if (isVowel(input[i])) {
-                vowelsInInput.add(input[i]);
-                vowelsMap[i] = 1;
+        while (left < right) {
+            if (!vowels.contains(s.charAt(left))) {
+                left++;
+            } else if (!vowels.contains(s.charAt(right))) {
+                right--;
             } else {
-                vowelsMap[i] = 0;
+                if (s.charAt(left) != s.charAt(right)) {
+                    builder.setCharAt(left, s.charAt(right));
+                    builder.setCharAt(right, s.charAt(left));
+                }
+                left++;
+                right--;
             }
         }
 
-        int index = vowelsInInput.size() - 1;
-        for (int i = 0; i < input.length; i++) {
-            if (vowelsMap[i] == 1) {
-                input[i] = vowelsInInput.get(index);
-                index--;
-            }
-        }
-
-        return String.valueOf(input);
-    }
-
-    private boolean isVowel(char c) {
-        for (char vowel : allVowels) {
-            if (c == vowel) {
-                return true;
-            }
-        }
-        return false;
+        return builder.toString();
     }
 }
