26a3c0d97f047f9329b8210d5c4746ecc2abb60b
mstoyanovca <mstoyanovca@gmail.com>
2026-09-21 20:08:04 -0400

    refactored KidsWithTheGreatestNumberOfCandies

--- src/main/java/leet_code_75/array_string/KidsWithTheGreatestNumberOfCandies.java
+++ src/main/java/leet_code_75/array_string/KidsWithTheGreatestNumberOfCandies.java
@@ -1,22 +1,17 @@
 package leet_code_75.array_string;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class KidsWithTheGreatestNumberOfCandies {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int greatestNumberOfCandies = Arrays.stream(candies).max().orElse(0);
+        int max = 0;
+        for (int i : candies) if (i > max) max = i;
+
         List<Boolean> result = new ArrayList<>();
-
-        for (int candy : candies) {
-            if (candy + extraCandies >= greatestNumberOfCandies) {
-                result.add(true);
-            } else {
-                result.add(false);
-            }
-        }
-
+        for (int i : candies)
+            if (i + extraCandies >= max) result.add(true);
+            else result.add(false);
         return result;
     }
 }
